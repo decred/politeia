764cebbd4decf6e29e48af689ca13f565e6f9f86
Sean Durkin <sndurkin@gmail.com>
2017-09-17 20:48:37 -0400

    Remove unused function.

--- politeiad/politeiad.go
+++ politeiad/politeiad.go
@@ -423,25 +423,6 @@ func (p *politeia) inventory(w http.ResponseWriter, r *http.Request) {
 	util.RespondWithJSON(w, http.StatusOK, reply)
 }
 
-func checkAuth(w http.ResponseWriter, r *http.Request) bool {
-	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
-	if len(s) != 2 {
-		return false
-	}
-
-	b, err := base64.StdEncoding.DecodeString(s[1])
-	if err != nil {
-		return false
-	}
-
-	pair := strings.SplitN(string(b), ":", 2)
-	if len(pair) != 2 {
-		return false
-	}
-
-	return pair[0] == "user" && pair[1] == "pass"
-}
-
 func (p *politeia) check(user, pass string) bool {
 	if user != p.cfg.RPCUser || pass != p.cfg.RPCPass {
 		return false
