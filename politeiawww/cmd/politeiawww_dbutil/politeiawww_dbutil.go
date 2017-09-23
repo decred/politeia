04819f762d36b3e49e3dd5af8994b3039f7572ba
Sean Durkin <sndurkin@gmail.com>
2017-09-22 22:46:43 -0400

    [www] Cleanup dbutil after review.

--- politeiawww/cmd/politeiawww_dbutil/politeiawww_dbutil.go
+++ politeiawww/cmd/politeiawww_dbutil/politeiawww_dbutil.go
@@ -2,55 +2,27 @@ package main
 
 import (
 	"encoding/hex"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/decred/politeia/politeiawww/database"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/chaincfg"
-	"github.com/decred/dcrutil"
+	"github.com/decred/politeia/politeiawww/database/localdb"
+	"github.com/decred/politeia/politeiawww/sharedconfig"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 )
 
-const (
-	defaultDataDirname = "data"
-	userdbPath         = "users"
-)
-
 var (
-	defaultHomeDir = dcrutil.AppDataDir("politeiawww", false)
-	defaultDataDir = filepath.Join(defaultHomeDir, defaultDataDirname)
-	dataDir        = flag.String("datadir", defaultDataDir, "Specify the politeiawww data directory.")
-	dumpDb         = flag.Bool("dump", false, "Dump the entire politeiawww database contents.")
-	setAdmin       = flag.Bool("setadmin", false, "Set the admin flag for a user. Parameters: <email> <true/false>")
-	testnet        = flag.Bool("testnet", false, "Whether to check the testnet database or not.")
-	dbDir          = ""
+	dataDir  = flag.String("datadir", sharedconfig.DefaultDataDir, "Specify the politeiawww data directory.")
+	dumpDb   = flag.Bool("dump", false, "Dump the entire politeiawww database contents.")
+	setAdmin = flag.Bool("setadmin", false, "Set the admin flag for a user. Parameters: <email> <true/false>")
+	testnet  = flag.Bool("testnet", false, "Whether to check the testnet database or not.")
+	dbDir    = ""
 )
-
-func decodeUser(payload []byte) (*database.User, error) {
-	var u database.User
-
-	err := json.Unmarshal(payload, &u)
-	if err != nil {
-		return nil, err
-	}
-
-	return &u, nil
-}
-
-func encodeUser(u database.User) ([]byte, error) {
-	b, err := json.Marshal(u)
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
-}
 
 func dumpAction() error {
 	userdb, err := leveldb.OpenFile(dbDir, &opt.Options{
@@ -67,7 +39,7 @@ func dumpAction() error {
 		key := iter.Key()
 		value := iter.Value()
 
-		u, err := decodeUser(value)
+		u, err := localdb.DecodeUser(value)
 		if err != nil {
 			return err
 		}
@@ -87,7 +59,7 @@ func setAdminAction() error {
 	}
 
 	email := args[0]
-	admin := args[1] == "true" || args[1] == "1"
+	admin := strings.ToLower(args[1]) == "true" || args[1] == "1"
 
 	userdb, err := leveldb.OpenFile(dbDir, &opt.Options{
 		ErrorIfMissing: true,
@@ -102,14 +74,14 @@ func setAdminAction() error {
 		fmt.Printf("User with email %v not found in the database", email)
 	}
 
-	u, err := decodeUser(b)
+	u, err := localdb.DecodeUser(b)
 	if err != nil {
 		return err
 	}
 
 	u.Admin = admin
 
-	b, err = encodeUser(*u)
+	b, err = localdb.EncodeUser(*u)
 	if err != nil {
 		return err
 	}
@@ -137,7 +109,7 @@ func _main() error {
 		net = chaincfg.MainNetParams.Name
 	}
 
-	dbDir = filepath.Join(*dataDir, net, userdbPath)
+	dbDir = filepath.Join(*dataDir, net, localdb.UserdbPath)
 	fmt.Printf("Database: %v\n", dbDir)
 
 	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
---
+++ politeiawww/sharedconfig/sharedconfig.go
@@ -0,0 +1,23 @@
+package sharedconfig
+
+import (
+	"path/filepath"
+
+	"github.com/decred/dcrutil"
+)
+
+const (
+	DefaultConfigFilename = "politeiawww.conf"
+	DefaultDataDirname    = "data"
+)
+
+var (
+	// DefaultHomeDir points to politeiawww's home directory for configuration and data.
+	DefaultHomeDir = dcrutil.AppDataDir("politeiawww", false)
+
+	// DefaultConfigFile points to politeiawww's default config file.
+	DefaultConfigFile = filepath.Join(DefaultHomeDir, DefaultConfigFilename)
+
+	// DefaultDataDir points to politeiawww's default data directory.
+	DefaultDataDir = filepath.Join(DefaultHomeDir, DefaultDataDirname)
+)
