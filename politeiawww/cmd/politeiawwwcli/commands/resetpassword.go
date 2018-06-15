fa830b03f6984b6c45a0a470ab0680b82f56ea2c
lukebp <lukebp@users.noreply.github.com>
2018-06-15 02:03:04 -0700

    politeiawww_refclient script (#327)
    * [wwwcli] add policy and faucet commands
    
    * convert politeiawww_refclient to bash script

--- politeiawww/cmd/politeiawwwcli/commands/resetpassword.go
+++ politeiawww/cmd/politeiawwwcli/commands/resetpassword.go
@@ -3,13 +3,11 @@ package commands
 type ResetpasswordCmd struct {
 	Args struct {
 		Email       string `positional-arg-name:"email"`
-		Password    string `positional-arg-name:"password"`
 		NewPassword string `positional-arg-name:"newpassword"`
 	} `positional-args:"true" required:"true"`
 }
 
 func (cmd *ResetpasswordCmd) Execute(args []string) error {
-	err := Ctx.ResetPassword(cmd.Args.Email, cmd.Args.Password,
-		cmd.Args.NewPassword)
+	err := Ctx.ResetPassword(cmd.Args.Email, cmd.Args.NewPassword)
 	return err
 }
