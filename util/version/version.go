8aee7122cc6dcfe2edccee6864eae423c9665eb8
Marco Peereboom <marco@peereboom.us>
2018-10-15 13:04:23 -0500

    Fix ldflags path, from @davecgh

--- util/version/version.go
+++ util/version/version.go
@@ -34,14 +34,14 @@ const (
 var (
 	// PreRelease is defined as a variable so it can be overridden during the
 	// build process with:
-	// '-ldflags "-X github.com/decred/dcrd/internal/version.PreRelease=foo"'
+	// '-ldflags "-X github.com/decred/politeia/util/version.PreRelease=foo"'
 	// if needed.  It MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
 	PreRelease = "pre"
 
 	// BuildMetadata is defined as a variable so it can be overridden during the
 	// build process with:
-	// '-ldflags "-X github.com/decred/dcrd/internal/version.BuildMetadata=foo"'
+	// '-ldflags "-X github.com/decred/politeia/util/version.BuildMetadata=foo"'
 	// if needed.  It MUST only contain characters from semanticBuildAlphabet
 	// per the semantic versioning spec.
 	BuildMetadata = "dev"
