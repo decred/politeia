af8c9d2cb21db4530e02949fd5addce5fe77d572
Sean Durkin <sndurkin@gmail.com>
2018-10-17 12:59:56 -0400

    [www] Remove HTML from templates.

--- politeiawww/templates.go
+++ politeiawww/templates.go
@@ -5,40 +5,47 @@
 package main
 
 const templateNewUserEmailRaw = `
-<div>Click the link below to verify your email and complete your registration:</div>
-<div style="margin: 20px 0 0 10px"><a href="{{.Link}}">{{.Link}}</a></div>
-<div style="margin-top: 20px">You are receiving this email because
-<span style="font-weight: bold">{{.Email}}</span> was used to register for Politeia.
-If you did not perform this action, please ignore this email.</div>
+Click the link below to verify your email and complete your registration:
+
+{{.Link}}
+
+You are receiving this email because {{.Email}} was used to register for Politeia.
+If you did not perform this action, please ignore this email.
 `
 
 const templateResetPasswordEmailRaw = `
-<div>Click the link below to continue resetting your password:</div>
-<div style="margin: 20px 0 0 10px"><a href="{{.Link}}">{{.Link}}</a></div>
-<div style="margin-top: 20px">You are receiving this email because a password reset
-was initiated for <span style="font-weight: bold">{{.Email}}</span> on Politeia.
-If you did not perform this action, please contact Politeia administrators.</div>
+Click the link below to continue resetting your password:
+
+{{.Link}}
+
+You are receiving this email because a password reset was initiated for {{.Email}}
+ on Politeia. If you did not perform this action, please contact Politeia
+administrators.
 `
 
 const templateUpdateUserKeyEmailRaw = `
-<div>Click the link below to verify your new identity:</div>
-<div style="margin: 20px 0 0 10px"><a href="{{.Link}}">{{.Link}}</a></div>
-<div style="margin-top: 20px">You are receiving this email because a new identity
-(public key: {{.PublicKey}}) was generated for
-<span style="font-weight: bold">{{.Email}}</span> on Politeia. If you did not perform
-this action, please contact Politeia administrators.</div>
+Click the link below to verify your new identity:
+
+{{.Link}}
+
+You are receiving this email because a new identity (public key: {{.PublicKey}})
+was generated for {{.Email}} on Politeia. If you did not perform this action,
+please contact Politeia administrators.
 `
 
 const templateUserLockedResetPasswordRaw = `
-<div>Your account was locked due to too many login attempts. You need to reset your password in order to unlock your account:</div>
-<div style="margin: 20px 0 0 10px"><a href="{{.Link}}">{{.Link}}</a></div>
-<div style="margin-top: 20px">You are receiving this email because someone made too many login attempts for <span style="font-weight: bold">{{.Email}}</span> on Politeia.</div>
-<div>If that was not you, please notify Politeia administrators.</div>
+Your account was locked due to too many login attempts. You need to reset your
+password in order to unlock your account:
+
+{{.Link}}
+
+You are receiving this email because someone made too many login attempts for
+{{.Email}} on Politeia. If that was not you, please notify Politeia administrators.
 `
 
 const templateNewProposalSubmittedRaw = `
-<div>A new proposal has been submitted on Politeia by <b>{{.Username}} ({{.Email}})</b>:</div>
-<div style="margin: 10px 0px;">
-<a  href="{{.Link}}" style="font-size:20px; font-weight:bold">{{.Name}}</a><br>
-</div>
+A new proposal has been submitted on Politeia by {{.Username}} ({{.Email}}):
+
+{{.Name}}
+{{.Link}}
 `
