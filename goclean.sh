7fdf5bda526af42955d4b970300c338230a28cb4
David Hill <dhill@mindcry.org>
2019-01-24 20:01:41 +0000

    [multi] switch to golangci-lint (#675)

--- goclean.sh
+++ goclean.sh
@@ -8,30 +8,18 @@
 # 6. race detector (http://blog.golang.org/race-detector)
 # 7. test coverage (http://blog.golang.org/cover)
 
-# gometalinter (github.com/alecthomas/gometalinter) is used to run each each
-# static checker.
-
 set -ex
 
 # run tests
 env GORACE="halt_on_error=1" go test -race ./...
 
-# Make sure gometalinter is installed and $GOPATH/bin is in your path.
-# $ go get -v github.com/alecthomas/gometalinter"
-# $ gometalinter --install"
-if [ ! -x "$(type -p gometalinter)" ]; then
-  exit 1
-fi
+# golangci-lint (github.com/golangci/golangci-lint) is used to run each each
+# static checker.
 
 # check linters
-# linters do not work with modules yet
-go mod vendor
-unset GO111MODULE
-
-gometalinter --vendor --disable-all --deadline=10m \
+golangci-lint run --disable-all --deadline=10m \
   --enable=gofmt \
   --enable=vet \
   --enable=gosimple \
   --enable=unconvert \
-  --enable=ineffassign \
-  ./...
+  --enable=ineffassign
