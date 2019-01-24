74a1a7917ff2160d1af2a90f22468a9705b6a4be
Seth <seth.benton@gmail.com>
2019-01-24 11:16:58 -0800

    wwwcli: Adds human readable settings edituser
    This commit adds human readable input settings to the edituser
    command. Help command text is also updated to reflect new
    human readable options.

--- politeiawww/cmd/politeiawwwcli/commands/edituser.go
+++ politeiawww/cmd/politeiawwwcli/commands/edituser.go
@@ -5,27 +5,31 @@
 package commands
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/decred/politeia/politeiawww/api/v1"
 )
 
 // Help message displayed for the command 'politeiawwwcli help edituser'
-var EditUserCmdHelpMsg = `edituser "userid" "action" "reason"
+var EditUserCmdHelpMsg = `edituser "emailnotifications"
 
-Edit the details for the given user id. 
+Edit email notifications for the currently logged in user. 
 
-Valid bit flags are:
-'1 << 0' - Notification email when status of user's proposal changes
-'1 << 1' - Notification email when voting has started on user's proposal
-'1 << 2' - Notification email when a proposal is vetted
-'1 << 3' - Notification email when a proposal is edited
-'1 << 4' - Notification email when voting on a proposal has started
-'1 << 5' - Notification email when a new proposal is submitted (admin only)
-'1 << 6' - Notification email when a user authorizes vote on proposal (admin only)
-'1 << 7' - Notification email when a comment is made on a user's proposal
-'1 << 8' - Notification email when a comment is made on a user's comment
-  
 Arguments:
-1. emailnotifications (uint64, optional)  Email notification setting (bit flag)
+1. emailnotifications       (string, required)   Notification option  
+
+Valid options are:
+
+1.   userproposalchange         Notify when status of my proposal changes
+2.   userproposalvotingstarted  Notify when my proposal vote has started
+4.   proposalvetted             Notify when any proposal is vetted
+8.   proposaledited             Notify when any proposal is edited
+16.  votingstarted              Notify when voting on any proposal has started
+32.  newproposal                Notify when proposal is submitted (admin only)
+64.  userauthorizedvote         Notify when user authorizes vote (admin only)
+128. commentonproposal          Notify when comment is made on my proposal
+256. commentoncomment           Notify when comment is made on my comment
 
 Request:
 {
@@ -36,17 +40,48 @@ Response:
 {}`
 
 type EditUserCmd struct {
-	EmailNotifications *uint64 `long:"emailnotifications" optional:"true" description:"Whether to notify via emails"`
+	Args struct {
+		NotifType string `positional-arg-name:"emailnotifications" description:"Email notifications"`
+	} `positional-args:"true" optional:"true"`
 }
 
 func (cmd *EditUserCmd) Execute(args []string) error {
+
+	EmailNotifs := map[string]v1.EmailNotificationT{
+		"userproposalchange":        1,
+		"userproposalvotingstarted": 2,
+		"proposalvetted":            4,
+		"proposaledited":            8,
+		"votingstarted":             16,
+		"newproposal":               32,
+		"userauthorizedvote":        64,
+		"commentonproposal":         128,
+		"commentoncomment":          256,
+	}
+
+	// Parse edit user option.  This can be either the numeric
+	// type code or the human readable equivalent.
+	var notif v1.EmailNotificationT
+	a, err := strconv.ParseUint(cmd.Args.NotifType, 10, 64)
+	if err == nil {
+		// Numeric action code found
+		notif = v1.EmailNotificationT(a)
+	} else if a, ok := EmailNotifs[cmd.Args.NotifType]; ok {
+		// Human readable action code found
+		// notif = v1.EmailNotificationT(a)
+		notif = a
+	} else {
+		return fmt.Errorf("Invalid edituser option. Type 'help edituser' for list of valid options")
+	}
+
 	// Setup request
+	helper := uint64(notif)
 	eu := &v1.EditUser{
-		EmailNotifications: cmd.EmailNotifications,
+		EmailNotifications: &helper, //cmd.EmailNotifications,
 	}
 
 	// Print request details
-	err := Print(eu, cfg.Verbose, cfg.RawJSON)
+	err = Print(eu, cfg.Verbose, cfg.RawJSON)
 	if err != nil {
 		return err
 	}
