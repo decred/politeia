a7ccbfe267166eba0e8ace25f025f80560e89157
Alex Yocom-Piatt <ay-p@decred.org>
2019-05-09 16:27:49 -0500

    [cms] Set email as temporary username

--- politeiawww/user.go
+++ politeiawww/user.go
@@ -2280,9 +2280,12 @@ func (p *politeiawww) processInviteNewUser(u cms.InviteNewUser) (*www.NewUserRep
 	}
 
 	// Create a new database user with the provided information.
+	// This temporarily sets the username to the given email.
+	// This will be overwritten during registration.
 	newUser := user.User{
-		Email: strings.ToLower(u.Email),
-		Admin: false,
+		Email:    strings.ToLower(u.Email),
+		Username: strings.ToLower(u.Email),
+		Admin:    false,
 	}
 	setInviteNewUserVerification(&newUser, token, expiry, false)
 
