e58f81cea57289d01afb51bac52ed4aace9babb8
Muharem Hrnjadovic <muharem@linux.com>
2019-09-24 18:09:47 +0200

    review: issue not needed

--- politeiawww/invoices.go
+++ politeiawww/invoices.go
@@ -1396,7 +1396,6 @@ func (p *politeiawww) processInvoiceComments(token string, u *user.User) (*www.G
 		return nil, err
 	}
 
-	// FIXME check for nil `u` pointer, GH issue #1011.
 	// Check to make sure the user is either an admin or the
 	// invoice author.
 	if !u.Admin && (ir.Username != u.Username) {
