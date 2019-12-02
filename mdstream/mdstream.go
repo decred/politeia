0119c4fffcf7216d2e4d1d03a78bb570904d44fc
Luke Powell <lukebp@users.noreply.github.com>
2019-12-02 12:42:21 -0700

    fix comments

--- mdstream/mdstream.go
+++ mdstream/mdstream.go
@@ -14,24 +14,25 @@ import (
 
 // XXX move remaining mdstreams out of politeiawww and into this package
 const (
-	// Markdown stream IDs
+	// mdstream IDs
 	IDProposalGeneral      = 0
 	IDProposalStatusChange = 2
 	// Note that 13 is in use by the decred plugin
 	// Note that 14 is in use by the decred plugin
 	// Note that 15 is in use by the decred plugin
-)
 
-// VersionProposalGeneral is the current supported proposal general version.
-const VersionProposalGeneral = 1
+	// mdstream current supported versions
+	VersionProposalGeneral      = 1
+	VersionProposalStatusChange = 1
+)
 
 // ProposalGeneral represents general metadata for a proposal.
 type ProposalGeneral struct {
 	Version   uint64 `json:"version"`   // Struct version
 	Timestamp int64  `json:"timestamp"` // Last update of proposal
-	Name      string `json:"name"`      // Generated proposal name
+	Name      string `json:"name"`      // Provided proposal name
 	PublicKey string `json:"publickey"` // Key used for signature
-	Signature string `json:"signature"` // Signature of merkle root
+	Signature string `json:"signature"` // Signature of proposal files merkle root
 }
 
 // EncodeProposalGeneral encodes a ProposalGeneral into a JSON byte slice.
@@ -53,16 +54,12 @@ func DecodeProposalGeneral(payload []byte) (*ProposalGeneral, error) {
 	return &md, nil
 }
 
-// VersionProposalStatusChange is the current supported proposal status change
-// version.
-const VersionProposalStatusChange = 1
-
 // ProposalStatusChange represents a proposal status change.
 // XXX this is missing the admin signature
 type ProposalStatusChange struct {
 	Version             uint             `json:"version"`                       // Version of the struct
 	AdminPubKey         string           `json:"adminpubkey"`                   // Identity of the administrator
-	NewStatus           pd.RecordStatusT `json:"newstatus"`                     // NewStatus
+	NewStatus           pd.RecordStatusT `json:"newstatus"`                     // New status
 	StatusChangeMessage string           `json:"statuschangemessage,omitempty"` // Status change message
 	Timestamp           int64            `json:"timestamp"`                     // Timestamp of the change
 }
