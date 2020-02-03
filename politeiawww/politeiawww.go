b4771e9d13fb0665bd832d85a3ce0b10d936512f
David Hill <dhill@mindcry.org>
2020-02-03 14:24:46 -0500

    politeiawww: set http headers (#1114)

--- politeiawww/politeiawww.go
+++ politeiawww/politeiawww.go
@@ -197,10 +197,15 @@ func (p *politeiawww) handleVersion(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Header().Add("Strict-Transport-Security",
+	w.Header().Set("Strict-Transport-Security",
 		"max-age=63072000; includeSubDomains")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Referrer-Policy", "same-origin")
+	w.Header().Set("X-Frame-Options", "DENY")
+	w.Header().Set("X-XSS-Protection", "1; mode=block")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set(www.CsrfToken, csrf.Token(r))
+
 	w.WriteHeader(http.StatusOK)
 	w.Write(vr)
 }
