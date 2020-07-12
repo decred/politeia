4d8887b9528851b184dff0f218869929bc8e9de4
amass <amassarwi@gmail.com>
2020-07-12 15:33:21 +0200

    add scripts/postgres/cmssetup.sh

--- scripts/postgres/cmssetup.sh
+++ scripts/postgres/cmssetup.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
-# This script sets up the PostgresSQL databases for the politeiad cache and
-# assigns user privileges.
+# This script sets up the PostgresSQL databases and assigns user privileges.
 # This script requires that you have already created PostgresSQL certificates
-# using the postgrescerts.sh script and that you have a PostgresSQL instance
+# using the postgrescerts.sh script and that you have a CockroachDB instance
 # listening on the default port localhost:5432.
 
 set -ex
 
 # POSTGRES_DIR must be the same directory that was used with the
-# postgrescerts.sh script.
+# cockroachcerts.sh script.
 POSTGRES_DIR=$1
 if [ "${POSTGRES_DIR}" == "" ]; then
   POSTGRES_DIR="${HOME}/.postgresql"
 fi
 
-# ROOT_CERTS_DIR must contain client.root.crt, client.root.key, and root.crt.
+# ROOT_CERTS_DIR must contain client.root.crt, client.root.key, and ca.crt.
 readonly ROOT_CERTS_DIR="${POSTGRES_DIR}/certs/clients/root"
 
 if [ ! -f "${ROOT_CERTS_DIR}/client.root.crt" ]; then
@@ -29,12 +28,11 @@ elif [ ! -f "${ROOT_CERTS_DIR}/root.crt" ]; then
 fi
 
 # Database names.
-readonly DB_MAINNET="records_mainnet"
-readonly DB_TESTNET="records_testnet3"
+readonly DB_MAINNET="cms_mainnet"
+readonly DB_TESTNET="cms_testnet3"
 
 # Database usernames.
-readonly USER_POLITEIAD="politeiad"
-readonly USER_POLITEIAWWW="politeiawww"
+readonly USER_POLITEIAWWW="politeiawww" 
 
 # Psql connection string
 readonly CONNECTION_STRING="host=localhost \
@@ -46,31 +44,14 @@ readonly CONNECTION_STRING="host=localhost \
   user=postgres \
   dbname=postgres"
 
-# Create the mainnet and testnet databases for the politeiad records cache.
+# Create the mainnet and testnet databases for the cms database.
 psql "$CONNECTION_STRING" \
   -c "CREATE DATABASE ${DB_MAINNET}"
 
 psql "$CONNECTION_STRING" \
   -c "CREATE DATABASE ${DB_TESTNET}"
 
-# Create the politeiad user(if not exists) and assign privileges.
-psql "$CONNECTION_STRING" \
-   -c "DO \$\$
-BEGIN
-CREATE USER ${USER_POLITEIAD};
-EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
-END
-\$\$;";
-
-psql "$CONNECTION_STRING" \
-  -c "GRANT CREATE \
-  ON DATABASE ${DB_MAINNET} TO ${USER_POLITEIAD}"
-
-psql "$CONNECTION_STRING" \
-  -c "GRANT CREATE \
-  ON DATABASE ${DB_TESTNET} TO ${USER_POLITEIAD}"
-
-# Create politeiawww user(if not exists) and assign privileges.
+# Create the politeiawww user (if not exists).
 psql "$CONNECTION_STRING" \
    -c "DO \$\$
 BEGIN
@@ -79,8 +60,11 @@ EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING E
 END
 \$\$;";
 
+# Assign privileges.
 psql "$CONNECTION_STRING" \
-  -c "GRANT CONNECT ON DATABASE ${DB_MAINNET} TO ${USER_POLITEIAWWW}"
+  -c "GRANT CREATE \
+  ON DATABASE ${DB_MAINNET} TO  ${USER_POLITEIAWWW}"
 
 psql "$CONNECTION_STRING" \
-  -c "GRANT CONNECT ON DATABASE ${DB_TESTNET} TO ${USER_POLITEIAWWW}"
+  -c "GRANT CREATE \
+  ON DATABASE ${DB_TESTNET} TO  ${USER_POLITEIAWWW}"
