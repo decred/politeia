1b29424cdc7beabde6d7d60d5e781420f7103dd2
Alex Yocom-Piatt <ay-p@decred.org>
2020-08-11 16:56:28 -0400

    whoops

--- scripts/cmsclean_code.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-  readonly ROOT_CERTS_DIR="${HOME}/.cockroachdb/certs/clients/root"
-
-  cockroach sql \
-    --certs-dir="${ROOT_CERTS_DIR}" \
-    --execute "DROP TABLE IF EXISTS users_testnet3.cms_code_stats"
-\
- 
