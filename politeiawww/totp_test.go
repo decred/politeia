e8235efc6eb8c5f9670ee114d51297d741f84918
Alex Yocom-Piatt <ay-p@decred.org>
2020-08-20 12:27:53 -0400

    make tests better

--- politeiawww/totp_test.go
+++ politeiawww/totp_test.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -12,130 +8,179 @@ import (
 	"github.com/pquerna/otp/totp"
 )
 
-func TestSetTOTP(t *testing.T) {
+func TestProcessSetTOTP(t *testing.T) {
 	p, cleanup := newTestPoliteiawww(t)
 	defer cleanup()
 
 	usr, _ := newUser(t, p, true, false)
 
 	var tests = []struct {
-		name       string
-		body       www.SetTOTP
-		setAgain   bool
-		wantStatus int
-		wantError  error
+		name   string
+		params www.SetTOTP
 	}{
 		{
-			"success set/verify",
+			"success",
 			www.SetTOTP{
 				Type: www.TOTPTypeBasic,
-			},
-			false,
-			http.StatusOK,
-			nil,
-		},
-		{
-			"error setting after set",
-			www.SetTOTP{
-				Type: www.TOTPTypeBasic,
-				Code: "12345",
-			},
-			true,
-			http.StatusBadRequest,
-			www.UserError{
-				ErrorCode: www.ErrorStatusTOTPFailedValidation,
 			},
 		},
 	}
 
 	for _, v := range tests {
 		t.Run(v.name, func(t *testing.T) {
-			// Prepare request and receive
-			r := newPostReq(t, www.RouteSetTOTP, v.body)
-			addSessionToReq(t, p, r, usr.ID.String())
-			w := httptest.NewRecorder()
-
-			p.handleSetTOTP(w, r)
-			res := w.Result()
-			body, _ := ioutil.ReadAll(res.Body)
-			res.Body.Close()
-
-			var gotReply www.SetTOTPReply
-			err := json.Unmarshal(body, &gotReply)
+			reply, err := p.processSetTOTP(v.params, usr)
 			if err != nil {
-				t.Errorf("unmarshal error with body %v", body)
+				t.Errorf("unable to set totp %v", err)
 			}
-
-			// Validate http status code
-			if res.StatusCode != v.wantStatus && !v.setAgain {
-				t.Errorf("got status code %v, want %v",
-					res.StatusCode, v.wantStatus)
-			}
-
-			code, err := totp.GenerateCode(gotReply.Key, time.Now())
+			userInfo, err := p.userByIDStr(usr.ID.String())
 			if err != nil {
-				t.Errorf("unable to generate code %v", err)
+				t.Errorf("unable to get update user %v", err)
 			}
+			if userInfo.TOTPSecret != reply.Key {
+				t.Error("secret returned does not match saved key")
+			}
+		})
+	}
+}
 
-			r = newPostReq(t, www.RouteVerifyTOTP, www.VerifyTOTP{
+func TestProcessSetTOTPAlreadySet(t *testing.T) {
+	p, cleanup := newTestPoliteiawww(t)
+	defer cleanup()
+
+	usr, _ := newUser(t, p, true, false)
+
+	key, err := totp.Generate(totp.GenerateOpts{
+		Issuer:      "politeia",
+		AccountName: usr.Username,
+	})
+	if err != nil {
+		t.Errorf("unable to generate secret key %v", err)
+	}
+
+	usr.TOTPType = int(www.TOTPTypeBasic)
+	usr.TOTPSecret = key.Secret()
+	usr.TOTPVerified = true
+	usr.TOTPLastUpdated = append(usr.TOTPLastUpdated, time.Now().Unix())
+
+	err = p.db.UserUpdate(*usr)
+	if err != nil {
+		t.Errorf("unable to update user secret key %v", err)
+	}
+
+	code, err := totp.GenerateCode(key.Secret(), time.Now())
+	if err != nil {
+		t.Errorf("unable to generate code %v", err)
+	}
+
+	var tests = []struct {
+		name      string
+		params    www.SetTOTP
+		wantError error
+	}{
+		{
+			"error wrong code",
+			www.SetTOTP{
+				Type: www.TOTPTypeBasic,
+				Code: "12345",
+			},
+			www.UserError{
+				ErrorCode: www.ErrorStatusTOTPFailedValidation,
+			},
+		},
+		{
+			"success",
+			www.SetTOTP{
+				Type: www.TOTPTypeBasic,
 				Code: code,
-			})
-			addSessionToReq(t, p, r, usr.ID.String())
-			w = httptest.NewRecorder()
+			},
+			nil,
+		},
+	}
 
-			p.handleVerifyTOTP(w, r)
-			res = w.Result()
-			body, _ = ioutil.ReadAll(res.Body)
-			res.Body.Close()
-
-			var gotVerifyReply www.VerifyTOTPReply
-			err = json.Unmarshal(body, &gotVerifyReply)
+	for _, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
+			reply, err := p.processSetTOTP(v.params, usr)
 			if err != nil {
-				t.Errorf("unmarshal error with body %v", body)
-			}
-
-			// Validate http status code
-			if res.StatusCode != v.wantStatus && !v.setAgain {
-				t.Errorf("got status code %v, want %v",
-					res.StatusCode, v.wantStatus)
-			}
-
-			if v.setAgain {
-				r = newPostReq(t, www.RouteVerifyTOTP, www.VerifyTOTP{
-					Code: v.body.Code,
-				})
-				addSessionToReq(t, p, r, usr.ID.String())
-				w = httptest.NewRecorder()
-
-				p.handleVerifyTOTP(w, r)
-				res = w.Result()
-				body, _ = ioutil.ReadAll(res.Body)
-				res.Body.Close()
-
-				err = json.Unmarshal(body, &gotReply)
-				if err != nil {
-					t.Errorf("unmarshal error with body %v", body)
-				}
-
-				// Validate http status code
-				if res.StatusCode != v.wantStatus {
-					t.Errorf("got status code %v, want %v",
-						res.StatusCode, v.wantStatus)
-					return
-				}
-
-				var ue www.UserError
-				err := json.Unmarshal(body, &ue)
-				if err != nil {
-					t.Errorf("unmarshal UserError: %v", err)
-				}
-
-				got := errToStr(ue)
+				got := errToStr(err)
 				want := errToStr(v.wantError)
 				if got != want {
-					t.Errorf("got error %v, want %v",
-						got, want)
+					t.Errorf("got %v, want %v", got, want)
 				}
+				return
+			}
+			userInfo, err := p.userByIDStr(usr.ID.String())
+			if err != nil {
+				t.Errorf("unable to get update user %v", err)
+			}
+			if userInfo.TOTPSecret != reply.Key {
+				t.Error("secret returned does not match saved key")
+			}
+		})
+	}
+}
+
+func TestProcessVerifyTOTP(t *testing.T) {
+	p, cleanup := newTestPoliteiawww(t)
+	defer cleanup()
+
+	usr, _ := newUser(t, p, true, false)
+
+	key, err := totp.Generate(totp.GenerateOpts{
+		Issuer:      "politeia",
+		AccountName: usr.Username,
+	})
+	if err != nil {
+		t.Errorf("unable to generate secret key %v", err)
+	}
+
+	usr.TOTPType = int(www.TOTPTypeBasic)
+	usr.TOTPSecret = key.Secret()
+	usr.TOTPVerified = false
+	usr.TOTPLastUpdated = append(usr.TOTPLastUpdated, time.Now().Unix())
+
+	err = p.db.UserUpdate(*usr)
+	if err != nil {
+		t.Errorf("unable to update user secret key %v", err)
+	}
+
+	code, err := totp.GenerateCode(key.Secret(), time.Now())
+	if err != nil {
+		t.Errorf("unable to generate code %v", err)
+	}
+
+	var tests = []struct {
+		name      string
+		params    www.VerifyTOTP
+		wantError error
+	}{
+		{
+			"error wrong code",
+			www.VerifyTOTP{
+				Code: "12345",
+			},
+			www.UserError{
+				ErrorCode: www.ErrorStatusTOTPFailedValidation,
+			},
+		},
+		{
+			"success",
+			www.VerifyTOTP{
+				Code: code,
+			},
+			nil,
+		},
+	}
+
+	for _, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
+			_, err := p.processVerifyTOTP(v.params, usr)
+			if err != nil {
+				got := errToStr(err)
+				want := errToStr(v.wantError)
+				if got != want {
+					t.Errorf("got %v, want %v", got, want)
+				}
+				return
 			}
 		})
 	}
