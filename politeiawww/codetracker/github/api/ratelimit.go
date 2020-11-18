fe12e8eef9eeaccaf2349cd20e43906bff47b453
Alex Yocom-Piatt <ay-p@decred.org>
2020-11-18 16:46:38 -0500

    cms: Fix ratelimiter after refactor

--- politeiawww/codetracker/github/api/ratelimit.go
+++ politeiawww/codetracker/github/api/ratelimit.go
@@ -21,35 +21,33 @@ func (a *Client) RateLimit() (RateLimitRule, error) {
 	a.Lock()
 
 	for {
-		if a.rateLimit.Remaining != 0 {
-			continue
+		if a.rateLimit.Remaining == 0 {
+			b, err := a.gh.Get(rateLimitURL)
+			if err != nil {
+				return RateLimitRule{}, err
+			}
+			bo, err := ioutil.ReadAll(b.Body)
+			b.Body.Close()
+			if err != nil {
+				return RateLimitRule{}, err
+			}
+			var apiRateLimit RateLimit
+			err = json.Unmarshal(bo, &apiRateLimit)
+			if err != nil {
+				return RateLimitRule{}, err
+			}
+			core := apiRateLimit.Resources.Core
+			if core.Remaining == 0 {
+				exp := time.Unix(core.Reset, 0)
+				dur := time.Until(exp)
+				log.Debugf("RATELIMIT REACHED - SLEEPING %v", dur)
+				time.Sleep(dur)
+				continue
+			}
+			log.Debugf("NEW RATELIMIT LOADED - %d remaining, exp %v",
+				core.Remaining, time.Unix(core.Reset, 0))
+			a.rateLimit = core
 		}
-		b, err := a.gh.Get(rateLimitURL)
-		if err != nil {
-			return RateLimitRule{}, err
-		}
-		bo, err := ioutil.ReadAll(b.Body)
-		b.Body.Close()
-		if err != nil {
-			return RateLimitRule{}, err
-		}
-		var apiRateLimit RateLimit
-		err = json.Unmarshal(bo, &apiRateLimit)
-		if err != nil {
-			return RateLimitRule{}, err
-		}
-		core := apiRateLimit.Resources.Core
-		if core.Remaining == 0 {
-			exp := time.Unix(core.Reset, 0)
-			dur := time.Until(exp)
-			log.Debugf("RATELIMIT REACHED - SLEEPING %v", dur)
-			time.Sleep(dur)
-			continue
-		}
-		log.Debugf("NEW RATELIMIT LOADED - %d remaining, exp %v",
-			core.Remaining, time.Unix(core.Reset, 0))
-		a.rateLimit = core
-
 		a.rateLimit.Remaining--
 		break
 	}
