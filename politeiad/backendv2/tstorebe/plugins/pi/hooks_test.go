0ff76fab8c57a57da5ce5af96af8ce05a522b429
luke <lukebp@users.noreply.github.com>
2021-07-06 16:56:02 -0500

    pi: Add proposal validation tests.

--- politeiad/backendv2/tstorebe/plugins/pi/hooks_test.go
+++ politeiad/backendv2/tstorebe/plugins/pi/hooks_test.go
@@ -4,7 +4,16 @@
 
 package pi
 
-import "testing"
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"testing"
+
+	backend "github.com/decred/politeia/politeiad/backendv2"
+	"github.com/decred/politeia/politeiad/plugins/pi"
+	"github.com/decred/politeia/util"
+)
 
 func TestProposalNameIsValid(t *testing.T) {
 	// Setup pi plugin
@@ -79,6 +88,189 @@ func TestProposalNameIsValid(t *testing.T) {
 			if isValid != test.want {
 				t.Errorf("got %v, want %v", isValid, test.want)
 			}
+		})
+	}
+}
+
+type proposalTest struct {
+	name string
+
+	files []backend.File // Input
+	err   error          // Output
+}
+
+func fileIndex() backend.File {
+	var (
+		text = `Hello, world. This is my proposal. It would be
+            really cool if you could approve it and pay me
+            all the moneys. DCR to the moon!`
+
+		payload = []byte(text)
+	)
+	return backend.File{
+		Name:    pi.FileNameIndexFile,
+		MIME:    "text/plain; charset=utf-8",
+		Digest:  hex.EncodeToString(util.Digest(payload)),
+		Payload: base64.StdEncoding.EncodeToString(payload),
+	}
+}
+
+// fileProposalMetadata returns a backend file for a proposal metadata. The
+// proposal metadata can optionally be provided as an argument. If no proposal
+// metadata is provided, one is created and filled with test data.
+func fileProposalMetadata(t *testing.T, pm *pi.ProposalMetadata) backend.File {
+	if pm == nil {
+		pm = &pi.ProposalMetadata{
+			Name: "Test Proposal Name",
+		}
+	}
+	pmb, err := json.Marshal(pm)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return backend.File{
+		Name:    pi.FileNameProposalMetadata,
+		MIME:    "text/plain; charset=utf-8",
+		Digest:  hex.EncodeToString(util.Digest(pmb)),
+		Payload: base64.StdEncoding.EncodeToString(pmb),
+	}
+}
+
+func filesNoAttachments(t *testing.T) []backend.File {
+	return []backend.File{
+		fileIndex(),
+		fileProposalMetadata(t, nil),
+	}
+}
+
+func filesWithProposalName(t *testing.T, name string) []backend.File {
+	return []backend.File{
+		fileIndex(),
+		fileProposalMetadata(t, &pi.ProposalMetadata{
+			Name: name,
+		}),
+	}
+}
+
+func proposalNameTests(t *testing.T) []proposalTest {
+	var (
+		nameTooLong = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+		errNameInvalid = backend.PluginError{
+			PluginID:  pi.PluginID,
+			ErrorCode: uint32(pi.ErrorCodeProposalNameInvalid),
+		}
+	)
+	return []proposalTest{
+		{
+			"proposal name empty",
+			filesWithProposalName(t, ""),
+			errNameInvalid,
+		},
+		{
+			"proposal name too short", // 7 characters
+			filesWithProposalName(t, "abcdefg"),
+			errNameInvalid,
+		},
+
+		// 81 characters
+		{
+			"proposal name too long", // 81 characters
+			filesWithProposalName(t, nameTooLong),
+			errNameInvalid,
+		},
+		{
+			"proposal name min length", // 8 characters
+			"12345678",
+			nil,
+		},
+		{
+			"valid title",
+			nil,
+		},
+		{
+			" - title: is valid; title. !.,  ",
+			nil,
+		},
+		{
+			" - title: is valid; title.   ",
+			nil,
+		},
+		{
+			"\n\n#This-is MY tittle###",
+			errNameInvalid,
+		},
+		{
+			"{this-is-the-title}",
+			errNameInvalid,
+		},
+		{
+			"\t<this- is-the title>",
+			errNameInvalid,
+		},
+		{
+			"{this   -is-the-title}   ",
+			errNameInvalid,
+		},
+		{
+			"###this is the title***",
+			errNameInvalid,
+		},
+		{
+			"###this is the title@+",
+			nil,
+		},
+	}
+}
+
+// proposalTests returns a list of tests that verify the files of a proposal
+// meet all formatting criteria that the pi plugin requires.
+func proposalTests() []proposalTest {
+	return []proposalTest{
+		{
+			"text file name invalid",
+			[]backend.File{},
+			pi.ErrorCodeTextFileNameInvalid,
+		},
+		{
+			"text file size invalid",
+			[]backend.File{},
+			pi.ErrorCodeTextFileSizeInvalid,
+		},
+		{
+			"image file size invalid",
+			[]backend.File{},
+			pi.ErrorCodeImageFileSizeInvalid,
+		},
+		{
+			"index file missing",
+			[]backend.File{},
+			pi.ErrorCodeTextFileMissing,
+		},
+		{
+			"too many images",
+			[]backend.File{},
+			pi.ErrorCodeImageFileCountInvalid,
+		},
+		{
+			"proposal metadata missing",
+			[]backend.File{},
+			pi.ErrorCodeTextFileMissing,
+		},
+		{
+			"proposal name invalid",
+			[]backend.File{},
+		},
+	}
+}
+
+func TestProposalFilesVerify(t *testing.T) {
+	var tests = []struct {
+		name string
+	}{}
+	for _, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
 		})
 	}
 }
