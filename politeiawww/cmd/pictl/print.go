f15b25e77c4f34f938f5a446f36f2d8a802d7ef2
Đàm Việt Anh <84569563+vibros68@users.noreply.github.com>
2021-08-20 21:08:35 +0700

    pictl: Improve human readable dollar printing.
    This diff adds a `dollars()` function to pictl that converts a int64
    representation of a USD value in cents into a human readable dollars
    string.
    
    Example: -13000 to "-$130.00"
    
    The `dollars()` function is used to print the `Amount` field of a
    proposal.

--- politeiawww/cmd/pictl/print.go
+++ politeiawww/cmd/pictl/print.go
@@ -5,7 +5,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/decred/politeia/util"
 )
@@ -51,4 +54,78 @@ func byteCountSI(b int64) string {
 // subsequent call will overwrite the existing text that was printed to stdout.
 func printInPlace(s string) {
 	fmt.Printf("\033[2K\r%s", s)
+}
+
+// dollars converts an int64 price in cents into a human readable dollar
+// string.
+//
+// | Input     | Output          |
+// |-----------------------------|
+// | 130000000 | "$1,300,000.00" |
+// | 13000000  | "$130,000.00"   |
+// | 130000    | "$1,300.00"     |
+// | 13000     | "$130.00"       |
+// | 78        | "$0.78"         |
+// | 9         | "$0.09"         |
+// | 0         | "$0.00"         |
+// | -9        | "-$0.09"        |
+// | -78       | "-$0.78"        |
+// | -13000000 | "-$130,000.00"  |
+func dollars(cents int64) string {
+	// Get the value in dollars.
+	dollars := float64(cents) / 100
+
+	// Initialize the buffer to store the string result.
+	var buf bytes.Buffer
+
+	// Check for a negative value.
+	if dollars < 0 {
+		buf.WriteString("-")
+		// Convert the negative value to a positive value.
+		// The code below can only handle positive values.
+		dollars = 0 - dollars
+	}
+	buf.WriteString("$")
+
+	// Convert the dollar value into a string and split it into a
+	// integer and decimal. This is done so that commas can be added
+	// to the integer.
+	var (
+		f       = strconv.FormatFloat(dollars, 'f', -1, 64)
+		s       = strings.Split(f, ".")
+		integer = s[0]
+
+		// The value may or may not have a decimal. Default to 0.
+		decimal = ".00"
+	)
+	if len(s) > 1 {
+		// The value includes a decimal. Overwrite the default.
+		decimal = "." + s[1]
+	}
+
+	// Write the integer to the buffer one character at a time. Commas
+	// are inserted in their appropriate places.
+	//
+	// Examples
+	// "100000" to "100,000"
+	// "1000000" to "1,000,000"
+	for i, c := range integer {
+		// A comma should be inserted if the character index is divisible
+		// by 3 when counting from the right side of the string.
+		divByThree := (len(integer)-i)%3 == 0
+
+		// A comma should never be inserted for the first character.
+		// Ex: "100000" should not be ",100,000"
+		if divByThree && i > 0 {
+			buf.WriteString(",")
+		}
+
+		// Write the character to the buffer.
+		buf.WriteRune(c)
+	}
+
+	// Write the decimal to the buffer.
+	buf.WriteString(decimal)
+
+	return buf.String()
 }
