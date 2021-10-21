5b673c79ff55ed22e82b9a2f366581c0b2cb87c6
lukebp <lukebp@users.noreply.github.com>
2021-10-21 09:23:00 -0500

    politeiawww: Move sessions into legacy package.
    The existing sessions store is tangled up with the legacy user database
    and politeia specific session values, so it's being moved into the
    legacy package. The implementation does not have proper separation of
    concerns and will not be usable by the new plugin model.

--- politeiawww/legacy/wwwuser.go
+++ politeiawww/legacy/wwwuser.go
@@ -11,7 +11,7 @@ import (
 	"net/http"
 
 	www "github.com/decred/politeia/politeiawww/api/www/v1"
-	"github.com/decred/politeia/politeiawww/sessions"
+	"github.com/decred/politeia/politeiawww/legacy/sessions"
 	"github.com/decred/politeia/util"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
