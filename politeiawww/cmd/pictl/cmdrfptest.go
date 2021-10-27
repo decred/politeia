53f3a57b0206da0867cdf2ae0bb801313b17624c
amass <amassarwi@gmail.com>
2021-10-28 00:53:12 +0300

    Approve RFP, submit submissions & start run off vote

--- politeiawww/cmd/pictl/cmdrfptest.go
+++ politeiawww/cmd/pictl/cmdrfptest.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	tkv1 "github.com/decred/politeia/politeiawww/api/ticketvote/v1"
+	pclient "github.com/decred/politeia/politeiawww/client"
+	"github.com/pkg/errors"
 )
 
 // cmdRFPTest runs tests to ensure the RFP workflow works as expected.
@@ -14,7 +16,8 @@ type cmdRFPTest struct {
 	Args struct {
 		AdminEmail    string `positional-arg-name:"adminemail" required:"true"`
 		AdminPassword string `positional-arg-name:"adminpassword" required:"true"`
-	} `positional-args:"true"`
+	} `positional-args:"true" required:"true"`
+	Password string `long:"password" optional:"true"`
 }
 
 // Execute executes the cmdRFPTest command.
@@ -23,7 +26,9 @@ type cmdRFPTest struct {
 func (c *cmdRFPTest) Execute(args []string) error {
 	const (
 		// sleepInterval is the time to wait in between requests
-		// when polling politeiawww for vote results.
+		// when polling the ticketvote API for vote results or when
+		// waiting for the RFP linkby deadline to expire before
+		// starting the runoff vote.
 		sleepInterval = 15 * time.Second
 	)
 
@@ -38,7 +43,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		return err
 	}
 	if policyWWW.PaywallEnabled {
-		return fmt.Errorf("paywall is not disabled")
+		return errors.Errorf("paywall is not disabled")
 	}
 
 	// Log start time
@@ -52,14 +57,14 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	fmt.Printf("  Login as admin\n")
 	err = userLogin(admin)
 	if err != nil {
-		return fmt.Errorf("failed to login admin: %v", err)
+		return errors.Errorf("failed to login admin: %v", err)
 	}
 	lr, err := client.Me()
 	if err != nil {
 		return err
 	}
 	if !lr.IsAdmin {
-		return fmt.Errorf("provided user is not an admin")
+		return errors.Errorf("provided user is not an admin")
 	}
 	admin.Username = lr.Username
 
@@ -67,7 +72,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	fmt.Printf("  Create a RFP\n")
 	r, err := proposalPublic(admin, admin, &proposalOpts{
 		Random: true,
-		RFP:    true,
+		LinkBy: "6m",
 	})
 	if err != nil {
 		return err
@@ -89,29 +94,39 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		return err
 	}
 
-	// Temporarily enable output to prompt user for password
-	cfg.Silent = false
-
 	// Cast votes on RFP
 	fmt.Printf("  Cast 'yes' votes\n")
-	err = castBallot(tokenRFP, "yes", "")
+
+	// Prompt the user for their password if they haven't already
+	// provided it.
+	password := c.Password
+	if password == "" {
+		// Temporarily enable output to prompt user for password
+		cfg.Silent = false
+		pass, err := promptWalletPassword()
+		if err != nil {
+			return err
+		}
+		password = string(pass)
+		cfg.Silent = true
+	}
+
+	err = castBallot(tokenRFP, "yes", password)
 	if err != nil {
 		return err
 	}
-
-	cfg.Silent = true
 
 	// Wait to RFP to finish voting
 	var vs tkv1.Summary
 	for vs.Status != tkv1.VoteStatusApproved &&
 		vs.Status != tkv1.VoteStatusRejected {
-		var c cmdVoteSummaries
-		c.Args.Tokens = []string{tokenRFP}
-		summaries, err := voteSummaries(&c)
+		// Fetch vote summary
+		var cvs cmdVoteSummaries
+		cvs.Args.Tokens = []string{tokenRFP}
+		summaries, err := voteSummaries(&cvs)
 		if err != nil {
 			return err
 		}
-
 		vs = summaries[tokenRFP]
 
 		fmt.Printf("  RFP voting still going on, block %v\\%v \n",
@@ -123,9 +138,135 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		// RFP approved, continue
 		fmt.Printf("  RFP approved successfully\n")
 	case tkv1.VoteStatusRejected:
-		return fmt.Errorf("wrong RFP vote status, want '%v', got '%v'",
+		return errors.Errorf("wrong RFP vote status, want '%v', got '%v'",
 			tkv1.VoteStatuses[tkv1.VoteStatusApproved],
 			tkv1.VoteStatuses[tkv1.VoteStatusRejected])
+	}
+
+	// Create 1 unvetted censored RFP submission
+	fmt.Printf("  Create 1 unvetted censored RFP submission\n")
+	r, err = proposalUnvettedCensored(admin, admin, &proposalOpts{
+		Random: true,
+		LinkTo: tokenRFP,
+	})
+	if err != nil {
+		return err
+	}
+	tokenUnvettedCensored := r.CensorshipRecord.Token
+
+	// Create 1 vetted censored RFP submission
+	fmt.Printf("  Create 1 vetted censored RFP submission\n")
+	r, err = proposalVettedCensored(admin, admin, &proposalOpts{
+		Random: true,
+		LinkTo: tokenRFP,
+	})
+	if err != nil {
+		return err
+	}
+	tokenVettedCensored := r.CensorshipRecord.Token
+
+	// Create 1 vetted abandoned RFP submission
+	fmt.Printf("  Create 1 vetted abandoned RFP submission\n")
+	r, err = proposalAbandoned(admin, admin, &proposalOpts{
+		Random: true,
+		LinkTo: tokenRFP,
+	})
+	if err != nil {
+		return err
+	}
+	tokenAbandoned := r.CensorshipRecord.Token
+
+	// Create 3 public RFP submissions
+	fmt.Printf("  Create 3 public RFP submissions\n")
+	var tokensPublic [3]string
+	r, err = proposalPublic(admin, admin, &proposalOpts{
+		Random: true,
+		LinkTo: tokenRFP,
+	})
+	if err != nil {
+		return err
+	}
+	tokensPublic[0] = r.CensorshipRecord.Token
+	r, err = proposalPublic(admin, admin, &proposalOpts{
+		Random: true,
+		LinkTo: tokenRFP,
+	})
+	if err != nil {
+		return err
+	}
+	tokensPublic[1] = r.CensorshipRecord.Token
+	r, err = proposalPublic(admin, admin, &proposalOpts{
+		Random: true,
+		LinkTo: tokenRFP,
+	})
+	if err != nil {
+		return err
+	}
+	tokensPublic[2] = r.CensorshipRecord.Token
+
+	// Get linkby unix value
+	var cps cmdProposals
+	cps.Args.Tokens = []string{tokenRFP}
+	ps, err := proposals(&cps)
+	if err != nil {
+		return err
+	}
+	p := ps[tokenRFP]
+	vm, err := pclient.VoteMetadataDecode(p.Files)
+	if err != nil {
+		return err
+	}
+	linkBy := vm.LinkBy
+
+	// Wait for the rfp deadline to expire
+	for linkBy > time.Now().Unix() {
+		fmt.Printf("  Waiting for the RFP deadline to expire\n")
+		time.Sleep(sleepInterval)
+	}
+
+	// Start runoff vote for the submissions
+	fmt.Printf("  Start runoff vote for the submissions\n")
+	err = voteRunoff(admin, tokenRFP, 1, 0, 0)
+	if err != nil {
+		return err
+	}
+
+	// Verify that the runoff vote contains only the 3 public proposals
+	fmt.Printf("  Verify that the runoff vote contains only the 3 public " +
+		"proposals\n")
+
+	// Fetch vote summaries of public proposals
+	var cvs cmdVoteSummaries
+	tokens := tokensPublic[:]
+	cvs.Args.Tokens = tokens
+	summaries, err := voteSummaries(&cvs)
+	if err != nil {
+		return err
+	}
+	// Ensure public proposals are voting
+	for _, t := range tokens {
+		s := summaries[t]
+		if s.Status != tkv1.VoteStatusStarted {
+			return errors.Errorf("public proposal %v invalid vote status, "+
+				"expected: %v, got: %v", t, tkv1.VoteStatuses[tkv1.VoteStatusStarted],
+				tkv1.VoteStatuses[s.Status])
+		}
+	}
+
+	// Fetch vote summaries of abandoned/consored proposals
+	tokens = []string{tokenUnvettedCensored, tokenVettedCensored, tokenAbandoned}
+	cvs.Args.Tokens = tokens
+	summaries, err = voteSummaries(&cvs)
+	if err != nil {
+		return err
+	}
+	// Ensure abandoned/censored proposals are not voting
+	for _, t := range tokens {
+		s := summaries[t]
+		if s.Status == tkv1.VoteStatusStarted {
+			return errors.Errorf("proposal %v has unexpected vote status: %v",
+				t, tkv1.VoteStatuses[s.Status])
+		}
 	}
 
 	return nil
@@ -139,4 +280,9 @@ Run tests to ensure the RFP workflow works as expected..
 Arguments:
 1. adminemail     (string, required)  Email for admin account.
 2. adminpassword  (string, required)  Password for admin account.
+
+Flags:
+ --password (string, optional) dcrwallet password. The user will be prompted
+                               for their password if one is not provided using
+                               this flag.
 `
--- politeiawww/cmd/pictl/testing.go
+++ politeiawww/cmd/pictl/testing.go
@@ -123,16 +123,36 @@ func proposalUnreviewed(u user, opts *proposalOpts) (*rcv1.Record, error) {
 	// Submit new proposal
 	var cn cmdProposalNew
 	if opts != nil {
-		cn.Name = opts.Name
-		cn.LinkTo = opts.LinkTo
-		cn.LinkBy = opts.LinkBy
-		cn.Amount = opts.Amount
-		cn.StartDate = opts.StartDate
-		cn.EndDate = opts.EndDate
-		cn.Domain = opts.Domain
-		cn.RFP = opts.RFP
-		cn.Random = opts.Random
-		cn.RandomImages = opts.RandomImages
+		if opts.Name != "" {
+			cn.Name = opts.Name
+		}
+		if opts.LinkTo != "" {
+			cn.LinkTo = opts.LinkTo
+		}
+		if opts.LinkBy != "" {
+			cn.LinkBy = opts.LinkBy
+		}
+		if opts.Amount != 0 {
+			cn.Amount = opts.Amount
+		}
+		if opts.StartDate != "" {
+			cn.StartDate = opts.StartDate
+		}
+		if opts.EndDate != "" {
+			cn.EndDate = opts.EndDate
+		}
+		if opts.Domain != "" {
+			cn.Domain = opts.Domain
+		}
+		if opts.RFP {
+			cn.RFP = opts.RFP
+		}
+		if opts.Random {
+			cn.Random = opts.Random
+		}
+		if opts.RandomImages {
+			cn.RandomImages = opts.RandomImages
+		}
 	}
 	r, err := proposalNew(&cn)
 	if err != nil {
@@ -140,16 +160,9 @@ func proposalUnreviewed(u user, opts *proposalOpts) (*rcv1.Record, error) {
 	}
 
 	// Edit the proposal
-	ce := cmdProposalEdit{
-		Random: true,
-	}
-	if opts != nil {
-		ce.RandomImages = opts.RandomImages
-	}
-	ce.Args.Token = r.CensorshipRecord.Token
-	r, err = proposalEdit(&ce)
+	r, err = proposalEditWithOpts(r.CensorshipRecord.Token, opts)
 	if err != nil {
-		return nil, fmt.Errorf("cmdProposalEdit: %v", err)
+		return nil, err
 	}
 
 	// Logout user
@@ -236,22 +249,61 @@ func proposalPublic(author, admin user, opts *proposalOpts) (*rcv1.Record, error
 	}
 
 	// Edit the proposal
-	ce := cmdProposalEdit{
-		Random: true,
-	}
-	if opts != nil {
-		ce.RandomImages = opts.RandomImages
-	}
-	ce.Args.Token = r.CensorshipRecord.Token
-	r, err = proposalEdit(&ce)
+	r, err = proposalEditWithOpts(r.CensorshipRecord.Token, opts)
 	if err != nil {
-		return nil, fmt.Errorf("cmdProposalEdit: %v", err)
+		return nil, err
 	}
 
 	// Logout author
 	err = userLogout()
 	if err != nil {
 		return nil, err
+	}
+
+	return r, nil
+}
+
+// proposalEditWithOpts edits a proposal, in addition to the proposal tokens, it
+// accepts a pointer to a proposalOpts struct which can be used to configure
+// the various arguments and flags of the cmdProposalEdit command.
+func proposalEditWithOpts(token string, opts *proposalOpts) (*rcv1.Record, error) {
+	var ce cmdProposalEdit
+	if opts != nil {
+		if opts.Name != "" {
+			ce.Name = opts.Name
+		}
+		if opts.LinkTo != "" {
+			ce.LinkTo = opts.LinkTo
+		}
+		if opts.LinkBy != "" {
+			ce.LinkBy = opts.LinkBy
+		}
+		if opts.Amount != 0 {
+			ce.Amount = opts.Amount
+		}
+		if opts.StartDate != "" {
+			ce.StartDate = opts.StartDate
+		}
+		if opts.EndDate != "" {
+			ce.EndDate = opts.EndDate
+		}
+		if opts.Domain != "" {
+			ce.Domain = opts.Domain
+		}
+		if opts.RFP {
+			ce.RFP = opts.RFP
+		}
+		if opts.Random {
+			ce.Random = opts.Random
+		}
+		if opts.RandomImages {
+			ce.RandomImages = opts.RandomImages
+		}
+	}
+	ce.Args.Token = token
+	r, err := proposalEdit(&ce)
+	if err != nil {
+		return nil, fmt.Errorf("cmdProposalEdit: %v", err)
 	}
 
 	return r, nil
@@ -383,6 +435,44 @@ func voteStart(admin user, token string, duration, quorum, pass uint32) error {
 
 	// Start the voting period
 	_, err = voteStartStandard(token, duration, quorum, pass, pc)
+	if err != nil {
+		return err
+	}
+
+	// Logout admin
+	err = userLogout()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// voteStartRunoff starts the runoff voting period on a RFP.
+//
+// This function returns with the admin logged out.
+func voteRunoff(admin user, token string, duration, quorum, pass uint32) error {
+	// Login admin
+	err := userLogin(admin)
+	if err != nil {
+		return err
+	}
+
+	// Setup client
+	opts := pclient.Opts{
+		HTTPSCert:  cfg.HTTPSCert,
+		Cookies:    cfg.Cookies,
+		HeaderCSRF: cfg.CSRF,
+		Verbose:    cfg.Verbose,
+		RawJSON:    cfg.RawJSON,
+	}
+	pc, err := pclient.New(cfg.Host, opts)
+	if err != nil {
+		return err
+	}
+
+	// Start the runoff voting period
+	_, err = voteStartRunoff(token, duration, quorum, pass, pc)
 	if err != nil {
 		return err
 	}
