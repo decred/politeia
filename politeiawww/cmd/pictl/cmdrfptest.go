decred/politeia

commit 63d933ced21589cd84c34b28bbe3eb3a13813779

pictl: Fix typo and improve logging in rfptest.
This commit fixes a typo in the `pictl rfptest` command and improves
the vote summaries logging of the runoff vote submissions.

politeiawww/cmd/pictl/cmdrfptest.go
@@ -1,3 +1,5 @@
+// Copyright (c) 2021 The Decred developers
+// Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 package main
@@ -144,7 +146,10 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	}
 
 	// Wait to RFP to finish voting
-	var approvedRFP bool
+	var (
+		approvedRFP bool
+		vs          tkv1.Summary
+	)
 	for !approvedRFP {
 		// Fetch vote summary
 		var cvs cmdVoteSummaries
@@ -153,7 +158,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		if err != nil {
 			return err
 		}
-		vs := summaries[tokenRFP]
+		vs = summaries[tokenRFP]
 
 		if vs.Status != tkv1.VoteStatusApproved {
 			fmt.Printf("  RFP voting still going on, block %v/%v \n",
@@ -164,6 +169,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		}
 	}
 	fmt.Printf("  RFP approved successfully\n")
+	fmt.Printf(voteSummaryString(tokenRFP, "    ", vs))
 
 	// Create 1 unvetted censored RFP submission
 	fmt.Printf("  Create 1 unvetted censored RFP submission\n")
@@ -292,7 +298,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	}
 
 	tokenSecond := tokensPublic[1]
-	err = castBallot(tokensPublic[1], "no", password)
+	err = castBallot(tokenSecond, "no", password)
 	if err != nil {
 		return err
 	}
@@ -307,7 +313,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		if err != nil {
 			return err
 		}
-		vs := summaries[tokenFirst]
+		vs = summaries[tokenFirst]
 
 		if vs.Status != tkv1.VoteStatusApproved {
 			fmt.Printf("  Runoff voting still going on, block %v/%v \n",
@@ -317,7 +323,8 @@ func (c *cmdRFPTest) Execute(args []string) error {
 			approvedSubmission = true
 		}
 	}
-	fmt.Printf("  First submission %v was approved successfully\n", tokenFirst)
+	fmt.Printf("  First submission was approved successfully\n")
+	fmt.Printf(voteSummaryString(tokenFirst, "    ", vs))
 
 	// Fetch vote summary of rejected proposal
 	cvs = cmdVoteSummaries{}
@@ -336,8 +343,13 @@ func (c *cmdRFPTest) Execute(args []string) error {
 				tkv1.VoteStatuses[s.Status])
 		}
 	}
-	fmt.Printf("  The other two submissions %v were rejected successfully\n",
-		tokens)
+	fmt.Printf("  The other two submissions were rejected successfully\n")
+	for i, t := range tokens {
+		fmt.Printf(voteSummaryString(t, "    ", summaries[t]))
+		if i != len(tokens)-1 {
+			fmt.Printf("    -----\n")
+		}
+	}
 
 	ts := timestampFromUnix(time.Now().Unix())
 	fmt.Printf("Done!\n")

politeiawww/cmd/pictl/cmdvotesummaries.go
@@ -53,7 +53,7 @@ func voteSummaries(c *cmdVoteSummaries) (map[string]tkv1.Summary, error) {
 
 	// Print summaries
 	for k, v := range sr.Summaries {
-		printVoteSummary(k, v)
+		printf(voteSummaryString(k, "", v))
 		printf("-----\n")
 	}
 

politeiawww/cmd/pictl/ticketvote.go
@@ -5,7 +5,9 @@
 package main
 
 import (
+	"fmt"
 	"sort"
+	"strings"
 
 	tkv1 "github.com/decred/politeia/politeiawww/api/ticketvote/v1"
 )
@@ -63,14 +65,18 @@ func printVoteResults(votes []tkv1.CastVoteDetails) {
 	}
 }
 
-func printVoteSummary(token string, s tkv1.Summary) {
-	printf("Token             : %v\n", token)
-	printf("Status            : %v\n", tkv1.VoteStatuses[s.Status])
+// voteSummaryString returns a string that contains the pretty printed vote
+// summary.
+func voteSummaryString(token, indent string, s tkv1.Summary) string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%vToken             : %v\n", indent, token))
+	sb.WriteString(fmt.Sprintf("%vStatus            : %v\n",
+		indent, tkv1.VoteStatuses[s.Status]))
 	switch s.Status {
 	case tkv1.VoteStatusUnauthorized, tkv1.VoteStatusAuthorized,
 		tkv1.VoteStatusIneligible:
 		// Nothing else to print
-		return
+		return sb.String()
 	}
 	var total uint64
 	for _, v := range s.Results {
@@ -78,19 +84,29 @@ func printVoteSummary(token string, s tkv1.Summary) {
 	}
 	quorum := int(float64(s.QuorumPercentage) / 100 * float64(s.EligibleTickets))
 	pass := int(float64(s.PassPercentage) / 100 * float64(total))
-	printf("Type              : %v\n", tkv1.VoteTypes[s.Type])
-	printf("Quorum Percentage : %v%% of eligible votes (%v votes)\n",
-		s.QuorumPercentage, quorum)
-	printf("Pass Percentage   : %v%% of cast votes (%v votes)\n",
-		s.PassPercentage, pass)
-	printf("Duration          : %v blocks\n", s.Duration)
-	printf("Start Block Hash  : %v\n", s.StartBlockHash)
-	printf("Start Block Height: %v\n", s.StartBlockHeight)
-	printf("End Block Height  : %v\n", s.EndBlockHeight)
-	printf("Eligible Tickets  : %v tickets\n", s.EligibleTickets)
-	printf("Best Block        : %v\n", s.BestBlock)
-	printf("Results\n")
+	sb.WriteString(fmt.Sprintf("%vType              : %v\n",
+		indent, tkv1.VoteTypes[s.Type]))
+	sb.WriteString(fmt.Sprintf("%vQuorum Percentage : %v%% of eligible votes "+
+		"(%v votes)\n", indent, s.QuorumPercentage, quorum))
+	sb.WriteString(fmt.Sprintf("%vPass Percentage   : %v%% of cast votes "+
+		" (%v votes)\n", indent, s.PassPercentage, pass))
+	sb.WriteString(fmt.Sprintf("%vDuration          : %v blocks\n",
+		indent, s.Duration))
+	sb.WriteString(fmt.Sprintf("%vStart Block Hash  : %v\n",
+		indent, s.StartBlockHash))
+	sb.WriteString(fmt.Sprintf("%vStart Block Height: %v\n",
+		indent, s.StartBlockHeight))
+	sb.WriteString(fmt.Sprintf("%vEnd Block Height  : %v\n",
+		indent, s.EndBlockHeight))
+	sb.WriteString(fmt.Sprintf("%vEligible Tickets  : %v tickets\n",
+		indent, s.EligibleTickets))
+	sb.WriteString(fmt.Sprintf("%vBest Block        : %v\n", indent,
+		s.BestBlock))
+	sb.WriteString(fmt.Sprintf("%vResults\n", indent))
 	for _, v := range s.Results {
-		printf(" %v %-3v %v votes\n", v.VoteBit, v.ID, v.Votes)
+		sb.WriteString(fmt.Sprintf("%v %v %-3v %v votes\n", indent, v.VoteBit,
+			v.ID, v.Votes))
 	}
+
+	return sb.String()
 }

commit 01bcb7b4a80e8a1aa7afb7c5da3497234d4da49a

pictl: Fix vote summary printing.
This commit fixes a bug that was introduced by 63d933ce that caused the
percent signs in the vote summary printing to be printed incorrectly.

`printf(voteSummaryString())`

The above code is same thing as:

`fmt.Printf("%\n") // Prints "%!(MISSING)"`

These should be:

`printf("%v\n", voteSummaryString())`

It also cleans up the `voteSummaryString()` function and replaces some
hardcoded arguments in the `rfptest` command with variables.

politeiawww/cmd/pictl/cmdrfptest.go
@@ -19,7 +19,9 @@ type cmdRFPTest struct {
 		AdminEmail    string `positional-arg-name:"adminemail" required:"true"`
 		AdminPassword string `positional-arg-name:"adminpassword" required:"true"`
 	} `positional-args:"true" required:"true"`
-	Password string `long:"password" optional:"true"`
+	Password         string `long:"password" optional:"true"`
+	QuorumPercentage uint32 `long:"quorumpercentage" optional:"true"`
+	PassPercentage   uint32 `long:"passpercentage" optional:"true"`
 }
 
 // Execute executes the cmdRFPTest command.
@@ -32,6 +34,16 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		// waiting for the RFP linkby deadline to expire before
 		// starting the runoff vote.
 		sleepInterval = 15 * time.Second
+
+		// quorumPerc is the percent of total votes required for a quorum.
+		quorumPerc uint32 = 0
+
+		// passPerc is the percent of cast votes required for approval.
+		passPerc uint32 = 1
+
+		// Vote options
+		voteOptionYes = "yes"
+		voteOptionNo  = "no"
 	)
 
 	// We don't want the output of individual commands printed.
@@ -118,7 +130,8 @@ func (c *cmdRFPTest) Execute(args []string) error {
 
 	// Start RFP vote
 	fmt.Printf("  Start vote on RFP\n")
-	err = voteStart(admin, tokenRFP, pr.VoteDurationMin, 0, 0)
+	err = voteStart(admin, tokenRFP, pr.VoteDurationMin,
+		quorumPerc, passPerc)
 	if err != nil {
 		return err
 	}
@@ -140,7 +153,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		cfg.Silent = true
 	}
 
-	err = castBallot(tokenRFP, "yes", password)
+	err = castBallot(tokenRFP, voteOptionYes, password)
 	if err != nil {
 		return err
 	}
@@ -169,7 +182,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		}
 	}
 	fmt.Printf("  RFP approved successfully\n")
-	fmt.Printf(voteSummaryString(tokenRFP, "    ", vs))
+	fmt.Printf("%v\n", voteSummaryString(tokenRFP, vs, "    "))
 
 	// Create 1 unvetted censored RFP submission
 	fmt.Printf("  Create 1 unvetted censored RFP submission\n")
@@ -241,7 +254,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 
 	// Start runoff vote for the submissions
 	fmt.Printf("  Start runoff vote for the submissions\n")
-	err = voteRunoff(admin, tokenRFP, pr.VoteDurationMin, 0, 0)
+	err = voteRunoff(admin, tokenRFP, pr.VoteDurationMin, quorumPerc, passPerc)
 	if err != nil {
 		return err
 	}
@@ -292,13 +305,13 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		" don't vote on third\n")
 
 	tokenFirst := tokensPublic[0]
-	err = castBallot(tokenFirst, "yes", password)
+	err = castBallot(tokenFirst, voteOptionYes, password)
 	if err != nil {
 		return err
 	}
 
 	tokenSecond := tokensPublic[1]
-	err = castBallot(tokenSecond, "no", password)
+	err = castBallot(tokenSecond, voteOptionNo, password)
 	if err != nil {
 		return err
 	}
@@ -324,7 +337,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		}
 	}
 	fmt.Printf("  First submission was approved successfully\n")
-	fmt.Printf(voteSummaryString(tokenFirst, "    ", vs))
+	fmt.Printf("%v\n", voteSummaryString(tokenFirst, vs, "    "))
 
 	// Fetch vote summary of rejected proposal
 	cvs = cmdVoteSummaries{}
@@ -345,7 +358,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	}
 	fmt.Printf("  The other two submissions were rejected successfully\n")
 	for i, t := range tokens {
-		fmt.Printf(voteSummaryString(t, "    ", summaries[t]))
+		fmt.Printf("%v\n", voteSummaryString(t, summaries[t], "    "))
 		if i != len(tokens)-1 {
 			fmt.Printf("    -----\n")
 		}

politeiawww/cmd/pictl/cmdvotesummaries.go
@@ -53,7 +53,7 @@ func voteSummaries(c *cmdVoteSummaries) (map[string]tkv1.Summary, error) {
 
 	// Print summaries
 	for k, v := range sr.Summaries {
-		printf(voteSummaryString(k, "", v))
+		printf("%v\n", voteSummaryString(k, v, ""))
 		printf("-----\n")
 	}
 

politeiawww/cmd/pictl/ticketvote.go
@@ -67,46 +67,88 @@ func printVoteResults(votes []tkv1.CastVoteDetails) {
 
 // voteSummaryString returns a string that contains the pretty printed vote
 // summary.
-func voteSummaryString(token, indent string, s tkv1.Summary) string {
-	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("%vToken             : %v\n", indent, token))
-	sb.WriteString(fmt.Sprintf("%vStatus            : %v\n",
-		indent, tkv1.VoteStatuses[s.Status]))
+//
+// Example output:
+// Token             : db9b06f36c21991c
+// Status            : rejected
+// Type              : standard
+// Quorum Percentage : 1% of eligible votes (46 votes)
+// Pass Percentage   : 50% of cast votes (30 votes)
+// Duration          : 1 blocks
+// Start Block Hash  : 000000007f1cacdef29710d0f95457364a9e4649904a0655d6ae9cec
+// Start Block Height: 800034
+// End Block Height  : 800051
+// Eligible Tickets  : 4638 tickets
+// Best Block        : 800051
+// Results
+//   1 yes 0 votes
+//   2 no  60 votes
+//
+// The indent argument can be used to add indentation to each line of the
+// string. An empty string will result in no indentation.
+func voteSummaryString(token string, s tkv1.Summary, indent string) string {
+	// Declare here to prevent goto errors
+	var (
+		sb strings.Builder
+
+		total  uint64 // Total votes cast
+		quorum int    // Votes required to reach a quorum
+		pass   int    // Votes required to pass
+	)
+
+	sb.WriteString(fmt.Sprintf("Token             : %v\n",
+		token))
+	sb.WriteString(fmt.Sprintf("Status            : %v\n",
+		tkv1.VoteStatuses[s.Status]))
 	switch s.Status {
 	case tkv1.VoteStatusUnauthorized, tkv1.VoteStatusAuthorized,
 		tkv1.VoteStatusIneligible:
 		// Nothing else to print
-		return sb.String()
+		goto addIndent
 	}
-	var total uint64
+
 	for _, v := range s.Results {
 		total += v.Votes
 	}
-	quorum := int(float64(s.QuorumPercentage) / 100 * float64(s.EligibleTickets))
-	pass := int(float64(s.PassPercentage) / 100 * float64(total))
-	sb.WriteString(fmt.Sprintf("%vType              : %v\n",
-		indent, tkv1.VoteTypes[s.Type]))
-	sb.WriteString(fmt.Sprintf("%vQuorum Percentage : %v%% of eligible votes "+
-		"(%v votes)\n", indent, s.QuorumPercentage, quorum))
-	sb.WriteString(fmt.Sprintf("%vPass Percentage   : %v%% of cast votes "+
-		" (%v votes)\n", indent, s.PassPercentage, pass))
-	sb.WriteString(fmt.Sprintf("%vDuration          : %v blocks\n",
-		indent, s.Duration))
-	sb.WriteString(fmt.Sprintf("%vStart Block Hash  : %v\n",
-		indent, s.StartBlockHash))
-	sb.WriteString(fmt.Sprintf("%vStart Block Height: %v\n",
-		indent, s.StartBlockHeight))
-	sb.WriteString(fmt.Sprintf("%vEnd Block Height  : %v\n",
-		indent, s.EndBlockHeight))
-	sb.WriteString(fmt.Sprintf("%vEligible Tickets  : %v tickets\n",
-		indent, s.EligibleTickets))
-	sb.WriteString(fmt.Sprintf("%vBest Block        : %v\n", indent,
+	quorum = int(float64(s.QuorumPercentage) / 100 * float64(s.EligibleTickets))
+	pass = int(float64(s.PassPercentage) / 100 * float64(total))
+
+	sb.WriteString(fmt.Sprintf("Type              : %v\n",
+		tkv1.VoteTypes[s.Type]))
+	sb.WriteString(fmt.Sprintf("Quorum Percentage : %v%% of eligible votes "+
+		"(%v votes)\n",
+		s.QuorumPercentage, quorum))
+	sb.WriteString(fmt.Sprintf("Pass Percentage   : %v%% of cast votes "+
+		"(%v votes)\n",
+		s.PassPercentage, pass))
+	sb.WriteString(fmt.Sprintf("Duration          : %v blocks\n",
+		s.Duration))
+	sb.WriteString(fmt.Sprintf("Start Block Hash  : %v\n",
+		s.StartBlockHash))
+	sb.WriteString(fmt.Sprintf("Start Block Height: %v\n",
+		s.StartBlockHeight))
+	sb.WriteString(fmt.Sprintf("End Block Height  : %v\n",
+		s.EndBlockHeight))
+	sb.WriteString(fmt.Sprintf("Eligible Tickets  : %v tickets\n",
+		s.EligibleTickets))
+	sb.WriteString(fmt.Sprintf("Best Block        : %v\n",
 		s.BestBlock))
-	sb.WriteString(fmt.Sprintf("%vResults\n", indent))
+	sb.WriteString(fmt.Sprintf("Results\n"))
 	for _, v := range s.Results {
-		sb.WriteString(fmt.Sprintf("%v %v %-3v %v votes\n", indent, v.VoteBit,
-			v.ID, v.Votes))
+		sb.WriteString(fmt.Sprintf("  %v %-3v %v votes\n",
+			v.VoteBit, v.ID, v.Votes))
 	}
 
-	return sb.String()
+addIndent:
+	// Add in indentation after each new line
+	r := strings.NewReplacer("\n", "\n"+indent)
+	ss := r.Replace(sb.String())
+
+	// Remove trailing spaces
+	ss = strings.TrimSpace(ss)
+
+	// Add indent to the first line
+	ss = indent + ss
+
+	return ss
 }