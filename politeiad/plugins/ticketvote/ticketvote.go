decred/politeia

commit 63d933ced21589cd84c34b28bbe3eb3a13813779

pictl: Fix typo and improve logging in rfptest.
This commit fixes a typo in the `pictl rfptest` command and improves
the vote summaries logging of the runoff vote submissions.

politeiawww/cmd/pictl/cmdrfptest.go
@@ -1,3 +1,5 @@
+// Copyright (c) 2021 The Decred developers
+// Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 package main
@@ -144,7 +146,10 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	}
 
 	// Wait to RFP to finish voting
-	var approvedRFP bool
+	var (
+		approvedRFP bool
+		vs          tkv1.Summary
+	)
 	for !approvedRFP {
 		// Fetch vote summary
 		var cvs cmdVoteSummaries
@@ -153,7 +158,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		if err != nil {
 			return err
 		}
-		vs := summaries[tokenRFP]
+		vs = summaries[tokenRFP]
 
 		if vs.Status != tkv1.VoteStatusApproved {
 			fmt.Printf("  RFP voting still going on, block %v/%v \n",
@@ -164,6 +169,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		}
 	}
 	fmt.Printf("  RFP approved successfully\n")
+	fmt.Printf(voteSummaryString(tokenRFP, "    ", vs))
 
 	// Create 1 unvetted censored RFP submission
 	fmt.Printf("  Create 1 unvetted censored RFP submission\n")
@@ -292,7 +298,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	}
 
 	tokenSecond := tokensPublic[1]
-	err = castBallot(tokensPublic[1], "no", password)
+	err = castBallot(tokenSecond, "no", password)
 	if err != nil {
 		return err
 	}
@@ -307,7 +313,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		if err != nil {
 			return err
 		}
-		vs := summaries[tokenFirst]
+		vs = summaries[tokenFirst]
 
 		if vs.Status != tkv1.VoteStatusApproved {
 			fmt.Printf("  Runoff voting still going on, block %v/%v \n",
@@ -317,7 +323,8 @@ func (c *cmdRFPTest) Execute(args []string) error {
 			approvedSubmission = true
 		}
 	}
-	fmt.Printf("  First submission %v was approved successfully\n", tokenFirst)
+	fmt.Printf("  First submission was approved successfully\n")
+	fmt.Printf(voteSummaryString(tokenFirst, "    ", vs))
 
 	// Fetch vote summary of rejected proposal
 	cvs = cmdVoteSummaries{}
@@ -336,8 +343,13 @@ func (c *cmdRFPTest) Execute(args []string) error {
 				tkv1.VoteStatuses[s.Status])
 		}
 	}
-	fmt.Printf("  The other two submissions %v were rejected successfully\n",
-		tokens)
+	fmt.Printf("  The other two submissions were rejected successfully\n")
+	for i, t := range tokens {
+		fmt.Printf(voteSummaryString(t, "    ", summaries[t]))
+		if i != len(tokens)-1 {
+			fmt.Printf("    -----\n")
+		}
+	}
 
 	ts := timestampFromUnix(time.Now().Unix())
 	fmt.Printf("Done!\n")

politeiawww/cmd/pictl/cmdvotesummaries.go
@@ -53,7 +53,7 @@ func voteSummaries(c *cmdVoteSummaries) (map[string]tkv1.Summary, error) {
 
 	// Print summaries
 	for k, v := range sr.Summaries {
-		printVoteSummary(k, v)
+		printf(voteSummaryString(k, "", v))
 		printf("-----\n")
 	}
 

politeiawww/cmd/pictl/ticketvote.go
@@ -5,7 +5,9 @@
 package main
 
 import (
+	"fmt"
 	"sort"
+	"strings"
 
 	tkv1 "github.com/decred/politeia/politeiawww/api/ticketvote/v1"
 )
@@ -63,14 +65,18 @@ func printVoteResults(votes []tkv1.CastVoteDetails) {
 	}
 }
 
-func printVoteSummary(token string, s tkv1.Summary) {
-	printf("Token             : %v\n", token)
-	printf("Status            : %v\n", tkv1.VoteStatuses[s.Status])
+// voteSummaryString returns a string that contains the pretty printed vote
+// summary.
+func voteSummaryString(token, indent string, s tkv1.Summary) string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%vToken             : %v\n", indent, token))
+	sb.WriteString(fmt.Sprintf("%vStatus            : %v\n",
+		indent, tkv1.VoteStatuses[s.Status]))
 	switch s.Status {
 	case tkv1.VoteStatusUnauthorized, tkv1.VoteStatusAuthorized,
 		tkv1.VoteStatusIneligible:
 		// Nothing else to print
-		return
+		return sb.String()
 	}
 	var total uint64
 	for _, v := range s.Results {
@@ -78,19 +84,29 @@ func printVoteSummary(token string, s tkv1.Summary) {
 	}
 	quorum := int(float64(s.QuorumPercentage) / 100 * float64(s.EligibleTickets))
 	pass := int(float64(s.PassPercentage) / 100 * float64(total))
-	printf("Type              : %v\n", tkv1.VoteTypes[s.Type])
-	printf("Quorum Percentage : %v%% of eligible votes (%v votes)\n",
-		s.QuorumPercentage, quorum)
-	printf("Pass Percentage   : %v%% of cast votes (%v votes)\n",
-		s.PassPercentage, pass)
-	printf("Duration          : %v blocks\n", s.Duration)
-	printf("Start Block Hash  : %v\n", s.StartBlockHash)
-	printf("Start Block Height: %v\n", s.StartBlockHeight)
-	printf("End Block Height  : %v\n", s.EndBlockHeight)
-	printf("Eligible Tickets  : %v tickets\n", s.EligibleTickets)
-	printf("Best Block        : %v\n", s.BestBlock)
-	printf("Results\n")
+	sb.WriteString(fmt.Sprintf("%vType              : %v\n",
+		indent, tkv1.VoteTypes[s.Type]))
+	sb.WriteString(fmt.Sprintf("%vQuorum Percentage : %v%% of eligible votes "+
+		"(%v votes)\n", indent, s.QuorumPercentage, quorum))
+	sb.WriteString(fmt.Sprintf("%vPass Percentage   : %v%% of cast votes "+
+		" (%v votes)\n", indent, s.PassPercentage, pass))
+	sb.WriteString(fmt.Sprintf("%vDuration          : %v blocks\n",
+		indent, s.Duration))
+	sb.WriteString(fmt.Sprintf("%vStart Block Hash  : %v\n",
+		indent, s.StartBlockHash))
+	sb.WriteString(fmt.Sprintf("%vStart Block Height: %v\n",
+		indent, s.StartBlockHeight))
+	sb.WriteString(fmt.Sprintf("%vEnd Block Height  : %v\n",
+		indent, s.EndBlockHeight))
+	sb.WriteString(fmt.Sprintf("%vEligible Tickets  : %v tickets\n",
+		indent, s.EligibleTickets))
+	sb.WriteString(fmt.Sprintf("%vBest Block        : %v\n", indent,
+		s.BestBlock))
+	sb.WriteString(fmt.Sprintf("%vResults\n", indent))
 	for _, v := range s.Results {
-		printf(" %v %-3v %v votes\n", v.VoteBit, v.ID, v.Votes)
+		sb.WriteString(fmt.Sprintf("%v %v %-3v %v votes\n", indent, v.VoteBit,
+			v.ID, v.Votes))
 	}
+
+	return sb.String()
 }

commit 128c45df6fde3d0ca1962bf353d623d99d04032a

pictl: Fix print bug and standardize vote params.
This commit fixes a bug that was introduced by 63d933ce that caused the
percent signs in the vote summary printing to be printed incorrectly.

`printf(voteSummaryString())`

The above code is same thing as:

`fmt.Printf("%\n") // Prints "%!(MISSING)"`

These should be:

`printf("%v\n", voteSummaryString())`

It also cleans up the `voteSummaryString()` function and standardizes
the vote parameters (duration, quorum, passing) used in various vote
commands.

politeiad/plugins/ticketvote/ticketvote.go
@@ -309,8 +309,8 @@ type VoteParams struct {
 	// the vote to meet a quorum.
 	QuorumPercentage uint32 `json:"quorumpercentage"`
 
-	// PassPercentage is the percent of total votes that are required
-	// to consider a vote option as passed.
+	// PassPercentage is the percent of cast votes required for a vote
+	// option to be considered as passing.
 	PassPercentage uint32 `json:"passpercentage"`
 
 	Options []VoteOption `json:"options"`

politeiawww/api/ticketvote/v1/v1.go
@@ -336,8 +336,8 @@ type VoteParams struct {
 	// the vote to meet a quorum.
 	QuorumPercentage uint32 `json:"quorumpercentage"`
 
-	// PassPercentage is the percent of total votes that are required
-	// to consider a vote option as passed.
+	// PassPercentage is the percent of cast votes required for a vote
+	// option to be considered as passing.
 	PassPercentage uint32 `json:"passpercentage"`
 
 	Options []VoteOption `json:"options"`
@@ -554,8 +554,8 @@ type Summary struct {
 	// vote in order to have a quorum.
 	QuorumPercentage uint32 `json:"quorumpercentage"`
 
-	// PassPercentage is the percent of total votes required to approve
-	// the vote in order for the vote to pass.
+	// PassPercentage is the percent of cast votes required for a vote
+	// option to be considered as passing.
 	PassPercentage uint32 `json:"passpercentage"`
 
 	Results []VoteResult `json:"results"`

politeiawww/cmd/pictl/cmdrfptest.go
@@ -16,12 +16,21 @@ import (
 // cmdRFPTest runs tests to ensure the RFP workflow works as expected.
 type cmdRFPTest struct {
 	Args struct {
-		AdminEmail    string `positional-arg-name:"adminemail" required:"true"`
-		AdminPassword string `positional-arg-name:"adminpassword" required:"true"`
+		AdminEmail    string `positional-arg-name:"adminemail"`
+		AdminPassword string `positional-arg-name:"adminpassword"`
 	} `positional-args:"true" required:"true"`
-	Password         string `long:"password" optional:"true"`
-	QuorumPercentage uint32 `long:"quorumpercentage" optional:"true"`
-	PassPercentage   uint32 `long:"passpercentage" optional:"true"`
+
+	// Password is the user's dcrwallet password.
+	Password string `long:"password"`
+
+	// Quorum is the percent of total votes required for a quorum. This is a
+	// pointer so that a value of 0 can be provided. A quorum of zero allows
+	// for the vote to be approved or rejected using a single DCR ticket.
+	Quorum *uint32 `long:"quorum"`
+
+	// Passing is the percent of cast votes required for a vote options to be
+	// considered as passing.
+	Passing uint32 `long:"passing"`
 }
 
 // Execute executes the cmdRFPTest command.
@@ -35,16 +44,25 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		// starting the runoff vote.
 		sleepInterval = 15 * time.Second
 
-		// quorumPerc is the percent of total votes required for a quorum.
-		quorumPerc uint32 = 0
-
-		// passPerc is the percent of cast votes required for approval.
-		passPerc uint32 = 1
-
 		// Vote options
 		voteOptionYes = "yes"
 		voteOptionNo  = "no"
 	)
+
+	// Setup vote parameters
+	var (
+		quorum  = defaultQuorum
+		passing = defaultPassing
+	)
+	if c.Quorum != nil {
+		quorum = *c.Quorum
+	}
+	if c.Passing != 0 {
+		passing = c.Passing
+	}
+
+	fmt.Printf("Quorum : %v%%\n", quorum)
+	fmt.Printf("Passing: %v%%\n", passing)
 
 	// We don't want the output of individual commands printed.
 	cfg.Verbose = false
@@ -131,7 +149,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	// Start RFP vote
 	fmt.Printf("  Start vote on RFP\n")
 	err = voteStart(admin, tokenRFP, pr.VoteDurationMin,
-		quorumPerc, passPerc)
+		quorum, passing)
 	if err != nil {
 		return err
 	}
@@ -182,7 +200,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		}
 	}
 	fmt.Printf("  RFP approved successfully\n")
-	fmt.Printf("%v\n", voteSummaryString(tokenRFP, vs, "    "))
+	fmt.Printf("%v\n", voteSummaryString(tokenRFP, vs, 4))
 
 	// Create 1 unvetted censored RFP submission
 	fmt.Printf("  Create 1 unvetted censored RFP submission\n")
@@ -254,7 +272,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 
 	// Start runoff vote for the submissions
 	fmt.Printf("  Start runoff vote for the submissions\n")
-	err = voteRunoff(admin, tokenRFP, pr.VoteDurationMin, quorumPerc, passPerc)
+	err = voteRunoff(admin, tokenRFP, pr.VoteDurationMin, quorum, passing)
 	if err != nil {
 		return err
 	}
@@ -337,7 +355,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 		}
 	}
 	fmt.Printf("  First submission was approved successfully\n")
-	fmt.Printf("%v\n", voteSummaryString(tokenFirst, vs, "    "))
+	fmt.Printf("%v\n", voteSummaryString(tokenFirst, vs, 4))
 
 	// Fetch vote summary of rejected proposal
 	cvs = cmdVoteSummaries{}
@@ -358,7 +376,7 @@ func (c *cmdRFPTest) Execute(args []string) error {
 	}
 	fmt.Printf("  The other two submissions were rejected successfully\n")
 	for i, t := range tokens {
-		fmt.Printf("%v\n", voteSummaryString(t, summaries[t], "    "))
+		fmt.Printf("%v\n", voteSummaryString(t, summaries[t], 4))
 		if i != len(tokens)-1 {
 			fmt.Printf("    -----\n")
 		}
@@ -381,7 +399,13 @@ Arguments:
 2. adminpassword  (string, required)  Password for admin account.
 
 Flags:
- --password (string, optional) dcrwallet password. The user will be prompted
-                               for their password if one is not provided using
-                               this flag.
+ --password (string) dcrwallet password. The user will be prompted for their
+                     password if one is not provided using this flag.
+ --quorum   (uint32) Percent of total votes required to reach a quorum. A
+                     quorum of 0 means that the vote can be approved or
+                     rejected using a single DCR ticket.
+                     (default: 0)
+ --passing  (uint32) Percent of cast votes required for a vote option to be
+                     considered as passing.
+                     (default: 60)
 `

politeiawww/cmd/pictl/cmdvotestart.go
@@ -18,21 +18,26 @@ import (
 )
 
 // cmdVoteStart starts the voting period on a record.
-//
-// QuorumPercentage and PassPercentage are strings and not uint32 so that a
-// value of 0 can be passed in and not be overwritten by the defaults. This is
-// sometimes desirable when testing.
 type cmdVoteStart struct {
 	Args struct {
-		Token            string `positional-arg-name:"token" required:"true"`
-		Duration         uint32 `positional-arg-name:"duration"`
-		QuorumPercentage uint32 `positional-arg-name:"quorumpercentage"`
-		PassPercentage   uint32 `positional-arg-name:"passpercentage"`
-	} `positional-args:"true"`
+		Token string `positional-arg-name:"token"`
+	} `positional-args:"true"  required:"true"`
+
+	// Duration is the duration, in blocks of the DCR ticket vote.
+	Duration uint32 `long:"duration"`
+
+	// Quorum is the percent of total votes required for a quorum. This is a
+	// pointer so that a value of 0 can be provided. A quorum of zero allows
+	// for the vote to be approved or rejected using a single DCR ticket.
+	Quorum *uint32 `long:"quorum"`
+
+	// Passing is the percent of cast votes required for a vote options to be
+	// considered as passing.
+	Passing uint32 `long:"passing"`
 
 	// Runoff is used to indicate the vote is a runoff vote and the
 	// provided token is the parent token of the runoff vote.
-	Runoff bool `long:"runoff" optional:"true"`
+	Runoff bool `long:"runoff"`
 }
 
 // Execute executes the cmdVoteStart command.
@@ -45,6 +50,23 @@ func (c *cmdVoteStart) Execute(args []string) error {
 	// start.
 	if cfg.Identity == nil {
 		return shared.ErrUserIdentityNotFound
+	}
+
+	// Setup the vote params. The default values
+	// are overridden if CLI flags are provided.
+	var (
+		duration = defaultDuration
+		quorum   = defaultQuorum
+		passing  = defaultPassing
+	)
+	if c.Duration > 0 {
+		duration = c.Duration
+	}
+	if c.Quorum != nil {
+		quorum = *c.Quorum
+	}
+	if c.Passing != 0 {
+		passing = c.Passing
 	}
 
 	// Setup client
@@ -60,31 +82,15 @@ func (c *cmdVoteStart) Execute(args []string) error {
 		return err
 	}
 
-	// Setup vote params
-	var (
-		// Default values
-		duration uint32 = 2016
-		quorum   uint32 = 20
-		pass     uint32 = 60
-	)
-	if c.Args.Duration != 0 {
-		duration = c.Args.Duration
-	}
-	if c.Args.QuorumPercentage != 0 {
-		quorum = c.Args.QuorumPercentage
-	}
-	if c.Args.PassPercentage != 0 {
-		pass = c.Args.PassPercentage
-	}
-
+	// Start the voting period
 	var sr *tkv1.StartReply
 	if c.Runoff {
-		sr, err = voteStartRunoff(token, duration, quorum, pass, pc)
+		sr, err = voteStartRunoff(token, duration, quorum, passing, pc)
 		if err != nil {
 			return err
 		}
 	} else {
-		sr, err = voteStartStandard(token, duration, quorum, pass, pc)
+		sr, err = voteStartStandard(token, duration, quorum, passing, pc)
 		if err != nil {
 			return err
 		}
@@ -223,21 +229,26 @@ func voteStartRunoff(parentToken string, duration, quorum, pass uint32, pc *pcli
 }
 
 // voteStartHelpMsg is printed to stdout by the help command.
-var voteStartHelpMsg = `votestart <token> <duration> <quorumpercentage> <passpercentage>
+var voteStartHelpMsg = `votestart <token>
 
-Start the voting period for a proposal. Requires admin privileges.
+Start a DCR ticket vote for a record. Requires admin privileges.
 
 If the vote is a runoff vote then the --runoff flag must be used. The provided
 token should be the parent token of the runoff vote.
 
 Arguments:
-1. token             (string, required)  Proposal censorship token
-2. duration          (uint32, optional)  Duration of vote in blocks
-                                         (default: 2016)
-3. quorumpercentage  (uint32, optional)  Percent of total votes required to
-                                         reach a quorum (default: 20)
-4. passpercentage    (uint32, optional)  Percent of cast votes required for
-                                         vote to be approved (default: 60)
+1. token (string, required) Record censorship token.
+
 Flags:
- --runoff  (bool, optional)  Start a runoff vote.
+ --duration (uint32) Duration, in blocks, of the vote.
+                     (default: 6)
+ --quorum   (uint32) Percent of total votes required to reach a quorum. A
+                     quorum of 0 means that the vote can be approved or
+                     rejected using a single DCR ticket.
+                     (default: 0)
+ --passing  (uint32) Percent of cast votes required for a vote option to be
+                     considered as passing.
+                     (default: 60)
+ --runoff  (bool)    The vote being started is a runoff vote.
+                     (default: false)
 `

politeiawww/cmd/pictl/cmdvotesummaries.go
@@ -53,7 +53,7 @@ func voteSummaries(c *cmdVoteSummaries) (map[string]tkv1.Summary, error) {
 
 	// Print summaries
 	for k, v := range sr.Summaries {
-		printf("%v\n", voteSummaryString(k, v, ""))
+		printf("%v\n", voteSummaryString(k, v, 0))
 		printf("-----\n")
 	}
 

politeiawww/cmd/pictl/cmdvotetestsetup.go
@@ -8,34 +8,39 @@ import (
 	"fmt"
 )
 
-// cmdVoteTestSetup sets up a batch of proposal votes.
+// cmdVoteTestSetup starts a batch of proposal votes.
 type cmdVoteTestSetup struct {
 	Args struct {
-		AdminEmail    string `positional-arg-name:"adminemail" required:"true"`
-		AdminPassword string `positional-arg-name:"adminpassword" required:"true"`
-	} `positional-args:"true"`
+		AdminEmail    string `positional-arg-name:"adminemail"`
+		AdminPassword string `positional-arg-name:"adminpassword"`
+	} `positional-args:"true" required:"true"`
 
-	// Options to adjust the vote params.
-	Votes            uint32 `long:"votes" optional:"true"`
-	Duration         uint32 `long:"duration" optional:"true"`
-	QuorumPercentage uint32 `long:"quorumpercentage" optional:"true"`
-	PassPercentage   uint32 `long:"passpercentage" optional:"true"`
+	// Votes is the number of DCR ticket votes that will be started.
+	Votes uint32 `long:"votes"`
 
-	// IncludeImages is used to include a random number of images when
-	// submitting proposals.
-	IncludeImages bool `long:"includeimages"`
+	// Duration is the duration, in blocks of the DCR ticket votes.
+	Duration uint32 `long:"duration"`
+
+	// Quorum is the percent of total votes required for a quorum. This is a
+	// pointer so that a value of 0 can be provided. A quorum of zero allows
+	// for the vote to be approved or rejected using a single DCR ticket.
+	Quorum *uint32 `long:"quorum"`
+
+	// Passing is the percent of cast votes required for a vote options to be
+	// considered as passing.
+	Passing uint32 `long:"passing"`
 }
 
 // Execute executes the cmdVoteTestSetup command.
 //
 // This function satisfies the go-flags Commander interface.
 func (c *cmdVoteTestSetup) Execute(args []string) error {
-	// Setup test parameters
+	// Setup vote parameters
 	var (
 		votes    uint32 = 10
-		duration uint32 = 6  // In blocks
-		quorum   uint32 = 1  // Percentage of total tickets
-		pass     uint32 = 50 // Percentage of votes cast
+		duration        = defaultDuration
+		quorum          = defaultQuorum
+		passing         = defaultPassing
 	)
 	if c.Votes > 0 {
 		votes = c.Votes
@@ -43,11 +48,11 @@ func (c *cmdVoteTestSetup) Execute(args []string) error {
 	if c.Duration > 0 {
 		duration = c.Duration
 	}
-	if c.QuorumPercentage > 0 {
-		quorum = c.QuorumPercentage
+	if c.Quorum != nil {
+		quorum = *c.Quorum
 	}
-	if c.PassPercentage > 0 {
-		pass = c.PassPercentage
+	if c.Passing != 0 {
+		passing = c.Passing
 	}
 
 	// We don't want the output of individual commands printed.
@@ -104,7 +109,7 @@ func (c *cmdVoteTestSetup) Execute(args []string) error {
 		}
 
 		// Start vote
-		err = voteStart(admin, token, duration, quorum, pass)
+		err = voteStart(admin, token, duration, quorum, passing)
 		if err != nil {
 			return err
 		}
@@ -117,7 +122,7 @@ func (c *cmdVoteTestSetup) Execute(args []string) error {
 // voteTestSetupHelpMsg is the printed to stdout by the help command.
 const voteTestSetupHelpMsg = `votetestsetup [flags] "adminemail" "adminpassword"
 
-Setup a batch of proposal votes. This command submits the specified number of
+Start batch of proposal votes. This command submits the specified number of
 proposals, makes them public, then starts the voting period on each one.
 
 Arguments:
@@ -125,13 +130,15 @@ Arguments:
 2. adminpassword  (string, required)  Password for admin account.
 
 Flags
- --votes         (uint32) Number of votes to start. (default: 10)
- --duration      (uint32) Duration of each vote in blocks. (default: 6)
- --quorum        (uint32) Percent of total votes required to reach a quorum.
-                          (default: 1)
- --pass          (uint32) Percent of votes cast required for the vote to be
-                          approved. (default: 50)
- --includeimages (bool)   Include images in proposal submissions. This will
-                          substantially increase the size of the proposal
-                          payload.
+ --votes    (uint32) Number of votes to start.
+                     (default: 10)
+ --duration (uint32) Duration, in blocks, of the vote.
+                     (default: 6)
+ --quorum   (uint32) Percent of total votes required to reach a quorum. A
+                     quorum of 0 means that the vote can be approved or
+                     rejected using a single DCR ticket.
+                     (default: 0)
+ --passing  (uint32) Percent of cast votes required for a vote option to be
+                     considered as passing.
+                     (default: 60)
 `

politeiawww/cmd/pictl/print.go
@@ -33,6 +33,37 @@ func printJSON(v interface{}) {
 	printf("%v\n", util.FormatJSON(v))
 }
 
+// printInPlace prints the provided text to stdout in a way that overwrites the
+// existing stdout text. This function can be called multiple times. Each
+// subsequent call will overwrite the existing text that was printed to stdout.
+func printInPlace(s string) {
+	fmt.Printf("\033[2K\r%s", s)
+}
+
+// addIndent adds indentation to the beginning of each line of the provided
+// string. The indentInSpaces argument is the number of spaces that will be
+// inserted into each line.
+//
+// Example: addIndent("hello,\nworld!\n", 2) -> "  hello,\n  world!\n"
+func addIndent(s string, indentInSpaces uint) string {
+	// Setup indent string
+	var b strings.Builder
+	for i := 0; i < int(indentInSpaces); i++ {
+		b.WriteString(" ")
+	}
+	indent := b.String()
+
+	// Add indentation after each new line
+	r := strings.NewReplacer("\n", "\n"+indent)
+	ss := r.Replace(s)
+
+	// Remove trailing spaces
+	ss = strings.TrimSpace(ss)
+
+	// Add indent to the first line
+	return indent + ss
+}
+
 // byteCountSI converts the provided bytes to a string representation of the
 // closest SI unit (kB, MB, GB, etc).
 func byteCountSI(b int64) string {
@@ -47,13 +78,6 @@ func byteCountSI(b int64) string {
 	}
 	return fmt.Sprintf("%.1f %cB",
 		float64(b)/float64(div), "kMGTPE"[exp])
-}
-
-// printInPlace prints the provided text to stdout in a way that overwrites the
-// existing stdout text. This function can be called multiple times. Each
-// subsequent call will overwrite the existing text that was printed to stdout.
-func printInPlace(s string) {
-	fmt.Printf("\033[2K\r%s", s)
 }
 
 // dollars converts an int64 price in cents into a human readable dollar

politeiawww/cmd/pictl/ticketvote.go
@@ -12,6 +12,18 @@ import (
 	tkv1 "github.com/decred/politeia/politeiawww/api/ticketvote/v1"
 )
 
+const (
+	// defaultDuration is the default duration, in blocks, of a DCR ticket vote.
+	defaultDuration uint32 = 6
+
+	// defaultQuorum is the default percent of total votes required for a quorum.
+	defaultQuorum uint32 = 0
+
+	// defaultPassing is the default percent of cast votes required for a vote
+	// option to be considered as passing.
+	defaultPassing uint32 = 60
+)
+
 func printAuthDetails(a tkv1.AuthDetails) {
 	printf("Token    : %v\n", a.Token)
 	printf("Action   : %v\n", a.Action)
@@ -85,16 +97,10 @@ func printVoteResults(votes []tkv1.CastVoteDetails) {
 //   2 no  60 votes
 //
 // The indent argument can be used to add indentation to each line of the
-// string. An empty string will result in no indentation.
-func voteSummaryString(token string, s tkv1.Summary, indent string) string {
-	// Declare here to prevent goto errors
-	var (
-		sb strings.Builder
-
-		total  uint64 // Total votes cast
-		quorum int    // Votes required to reach a quorum
-		pass   int    // Votes required to pass
-	)
+// string. The provided number of spaces will be inserted into the beginning
+// of each line.
+func voteSummaryString(token string, s tkv1.Summary, indentInSpaces uint) string {
+	var sb strings.Builder
 
 	sb.WriteString(fmt.Sprintf("Token             : %v\n",
 		token))
@@ -104,9 +110,14 @@ func voteSummaryString(token string, s tkv1.Summary, indent string) string {
 	case tkv1.VoteStatusUnauthorized, tkv1.VoteStatusAuthorized,
 		tkv1.VoteStatusIneligible:
 		// Nothing else to print
-		goto addIndent
+		return addIndent(sb.String(), indentInSpaces)
 	}
 
+	var (
+		total  uint64 // Total votes cast
+		quorum int    // Votes required to reach a quorum
+		pass   int    // Votes required to pass
+	)
 	for _, v := range s.Results {
 		total += v.Votes
 	}
@@ -139,16 +150,5 @@ func voteSummaryString(token string, s tkv1.Summary, indent string) string {
 			v.VoteBit, v.ID, v.Votes))
 	}
 
-addIndent:
-	// Add in indentation after each new line
-	r := strings.NewReplacer("\n", "\n"+indent)
-	ss := r.Replace(sb.String())
-
-	// Remove trailing spaces
-	ss = strings.TrimSpace(ss)
-
-	// Add indent to the first line
-	ss = indent + ss
-
-	return ss
+	return addIndent(sb.String(), indentInSpaces)
 }