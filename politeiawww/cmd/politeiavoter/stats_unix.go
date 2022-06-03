a2cf64aa8a066aaf3af1829d19e1992950f6e560
Dave Collins <davec@conformal.com>
2022-06-03 11:05:22 -0500

    pivoter: Correct unix build tags.
    The semantics between the old build tag style and new build tag style
    aren't exactly the same and they were incorrectly updated for
    politeiavoter by commit
    af61db45ee9605e6d8646a0b53923a5ea4b6d939.

--- politeiawww/cmd/politeiavoter/stats_unix.go
+++ politeiawww/cmd/politeiavoter/stats_unix.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 //
-// go:build !windows
+//go:build !windows
+// +build !windows
 
 package main
 
