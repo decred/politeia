decred/politeia

commit 2e41e043915dc5cb50473be1c41bb87068fc1a2e

Add tstore backend and plugin architecture.
This diff rewrites much of the politeia codebase. A summary of the major
changes are listed below.

**politeiad tstore backend**

A new backend was added to politeiad that is referred to as the tstore backend.
It combines a trillian log, commonly referred to as a tlog, and a key-value
store. The tstore backend differs from the git backend in three main ways.

1. Scalability

	The git backend relied on git repos saved to the file system to store data.
	This approach cannot be scaled out since it is constrained to the file system
	of a single politeiad instances. The tstore backend moves the data into an
	actual database, allowing for much greater scalability.

2. Separate the timestamps and the data blobs

	The git backend would timestamp the git commit hashes onto the decred
	blockchain. These commit hashes are merkle roots of all the files in the
	commit. The timestamp and the data blobs in this setup are interconnected. If
	you delete a data blob from the commit history, such as if you need to censor
	a public comment, you destroy the ability to recreate the merkle path for
	other data blobs included in that commit merkle root, thus destroying you're
	ability to prove inclusion of data in a decred timestamp. Your only option is
	to remove the data from the git repo in a new commit, but the censored data
	remains in the git history. The tstore backend seperates out the timestamps
	from the data blobs. A merkle tree of the data is stored in a tlog instance
	while the data itself is stored in a key-value database. This means we can
	delete data from the database, such as if you need to censor a public
	comment, without impacting any of the timestamps or our ability to prove
	the inclusion of a piece of data in a timestamp.

3. Ability to retrieve timestamp inclusion proofs

	The tstore backend gives us the ability to retrieve an inclusion proof for

politeiad/plugins/comments/comments.go
@@ -1,0 +1,439 @@
+// Copyright (c) 2020-2021 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// Package comments provides a plugin for extending a record with comment
+// functionality.
+package comments
+
+const (
+	// PluginID is the unique identifier for this plugin.
+	PluginID = "comments"
+
+	// Plugin commands
+	CmdNew        = "new"        // Create a new comment
+	CmdEdit       = "edit"       // Edit a comment
+	CmdDel        = "del"        // Del a comment
+	CmdVote       = "vote"       // Vote on a comment
+	CmdGet        = "get"        // Get specified comments
+	CmdGetAll     = "getall"     // Get all comments for a record
+	CmdGetVersion = "getversion" // Get specified version of a comment
+	CmdCount      = "count"      // Get comments count for a record
+	CmdVotes      = "votes"      // Get comment votes
+	CmdTimestamps = "timestamps" // Get timestamps
+)
+
+// Plugin setting keys can be used to specify custom plugin settings. Default
+// plugin setting values can be overridden by providing a plugin setting key
+// and value to the plugin on startup.
+const (
+	// SettingKeyCommentLengthMax is the plugin setting key for the
+	// SettingCommentLengthMax plugin setting.
+	SettingKeyCommentLengthMax = "commentlengthmax"
+
+	// SettingKeyVoteChangesMax is the plugin setting key for the
+	// SettingVoteChangesMax plugin setting.
+	SettingKeyVoteChangesMax = "votechangesmax"
+)
+
+// Plugin setting default values. These can be overridden by providing a plugin
+// setting key and value to the plugin on startup.
+const (
+	// SettingCommentLengthMax is the default maximum number of
+	// characters that are allowed in a comment.
+	SettingCommentLengthMax uint32 = 8000
+
+	// SettingVoteChangesMax is the default maximum number of times a
+	// user can change their vote on a comment. This prevents a
+	// malicious user from being able to spam comment votes.
+	SettingVoteChangesMax uint32 = 5
+)
+
+// ErrorCodeT represents a error that was caused by the user.
+type ErrorCodeT uint32
+
+const (
+	// ErrorCodeInvalid is an invalid error code.
+	ErrorCodeInvalid ErrorCodeT = 0
+
+	// ErrorCodeTokenInvalid is returned when a token is invalid.
+	ErrorCodeTokenInvalid ErrorCodeT = 1
+
+	// ErrorCodePublicKeyInvalid is returned when a public key is
+	// invalid.
+	ErrorCodePublicKeyInvalid ErrorCodeT = 2
+
+	// ErrorCodeSignatureInvalid is returned when a signature is
+	// invalid.
+	ErrorCodeSignatureInvalid ErrorCodeT = 3
+
+	// ErrorCodeMaxLengthExceeded is returned when a comment exceeds the
+	// max length plugin setting.
+	ErrorCodeMaxLengthExceeded ErrorCodeT = 4
+
+	// ErrorCodeNoChanges is returned when a comment edit does not
+	// contain any changes.
+	ErrorCodeNoChanges ErrorCodeT = 5
+
+	// ErrorCodeCommentNotFound is returned when a comment could not be
+	// found.
+	ErrorCodeCommentNotFound ErrorCodeT = 6
+
+	// ErrorCodeUserUnauthorized is returned when a user is attempting
+	// to edit a comment that they did not submit.
+	ErrorCodeUserUnauthorized ErrorCodeT = 7
+
+	// ErrorCodeParentIDInvalid is returned when a comment parent ID
+	// does not correspond to an actual comment.
+	ErrorCodeParentIDInvalid ErrorCodeT = 8
+
+	// ErrorCodeVoteInvalid is returned when a comment vote is invalid.
+	ErrorCodeVoteInvalid ErrorCodeT = 9
+
+	// ErrorCodeVoteChangesMaxExceeded is returned when the number of
+	// times the user has changed their vote has exceeded the vote
+	// changes max plugin setting.
+	ErrorCodeVoteChangesMaxExceeded ErrorCodeT = 10
+
+	// ErrorCodeRecordStateInvalid is returned when the provided state
+	// does not match the record state.
+	ErrorCodeRecordStateInvalid ErrorCodeT = 11
+)
+
+var (
+	// ErrorCodes contains the human readable error messages.
+	ErrorCodes = map[ErrorCodeT]string{
+		ErrorCodeInvalid:                "error code invalid",
+		ErrorCodeTokenInvalid:           "token invalid",
+		ErrorCodePublicKeyInvalid:       "public key invalid",
+		ErrorCodeSignatureInvalid:       "signature invalid",
+		ErrorCodeMaxLengthExceeded:      "max length exceeded",
+		ErrorCodeNoChanges:              "no changes",
+		ErrorCodeCommentNotFound:        "comment not found",
+		ErrorCodeUserUnauthorized:       "user unauthorized",
+		ErrorCodeParentIDInvalid:        "parent id invalid",
+		ErrorCodeVoteInvalid:            "vote invalid",
+		ErrorCodeVoteChangesMaxExceeded: "vote changes max exceeded",
+		ErrorCodeRecordStateInvalid:     "record state invalid",
+	}
+)
+
+// RecordStateT represents the state of a record.
+type RecordStateT uint32
+
+const (
+	// RecordStateInvalid is an invalid record state.
+	RecordStateInvalid RecordStateT = 0
+
+	// RecordStateUnvetted indicates a record has not been made public.
+	RecordStateUnvetted RecordStateT = 1
+
+	// RecordStateVetted indicates a record has been made public.
+	RecordStateVetted RecordStateT = 2
+)
+
+// Comment represent a record comment.
+//
+// A parent ID of 0 indicates that the comment is a base level comment and not
+// a reply commment.
+//
+// Comments made on a record when it is unvetted and when it is vetted are
+// treated as two distinct groups of comments. When a record becomes vetted the
+// comment ID starts back at 1.
+//
+// Signature is the client signature of State+Token+ParentID+Comment.
+type Comment struct {
+	UserID    string       `json:"userid"`    // Unique user ID
+	State     RecordStateT `json:"state"`     // Record state
+	Token     string       `json:"token"`     // Record token
+	ParentID  uint32       `json:"parentid"`  // Parent comment ID if reply
+	Comment   string       `json:"comment"`   // Comment text
+	PublicKey string       `json:"publickey"` // Public key used for Signature
+	Signature string       `json:"signature"` // Client signature
+	CommentID uint32       `json:"commentid"` // Comment ID
+	Version   uint32       `json:"version"`   // Comment version
+	Timestamp int64        `json:"timestamp"` // UNIX timestamp of last edit
+	Receipt   string       `json:"receipt"`   // Server sig of client sig
+	Downvotes uint64       `json:"downvotes"` // Tolal downvotes on comment
+	Upvotes   uint64       `json:"upvotes"`   // Total upvotes on comment
+
+	Deleted bool   `json:"deleted,omitempty"` // Comment has been deleted
+	Reason  string `json:"reason,omitempty"`  // Reason for deletion
+
+	// Optional fields to be used freely
+	ExtraData     string `json:"extradata,omitempty"`
+	ExtraDataHint string `json:"extradatahint,omitempty"`
+}
+
+// CommentAdd is the structure that is saved to disk when a comment is created
+// or edited.
+//
+// Signature is the client signature of State+Token+ParentID+Comment.
+type CommentAdd struct {
+	// Data generated by client
+	UserID    string       `json:"userid"`    // Unique user ID
+	State     RecordStateT `json:"state"`     // Record state
+	Token     string       `json:"token"`     // Record token
+	ParentID  uint32       `json:"parentid"`  // Parent comment ID
+	Comment   string       `json:"comment"`   // Comment
+	PublicKey string       `json:"publickey"` // Pubkey used for Signature
+	Signature string       `json:"signature"` // Client signature
+
+	// Metadata generated by server
+	CommentID uint32 `json:"commentid"` // Comment ID
+	Version   uint32 `json:"version"`   // Comment version
+	Timestamp int64  `json:"timestamp"` // Received UNIX timestamp
+	Receipt   string `json:"receipt"`   // Server signature of client signature
+
+	// Optional fields to be used freely
+	ExtraData     string `json:"extradata,omitempty"`
+	ExtraDataHint string `json:"extradatahint,omitempty"`
+}
+
+// CommentDel is the structure that is saved to disk when a comment is deleted.
+// Some additional fields like ParentID and UserID are required to be saved
+// since all the CommentAdd records will be deleted and the client needs these
+// additional fields to properly display the deleted comment in the comment
+// hierarchy.
+//
+// Signature is the client signature of the State+Token+CommentID+Reason
+type CommentDel struct {
+	// Data generated by client
+	Token     string       `json:"token"`     // Record token
+	State     RecordStateT `json:"state"`     // Record state
+	CommentID uint32       `json:"commentid"` // Comment ID
+	Reason    string       `json:"reason"`    // Reason for deleting
+	PublicKey string       `json:"publickey"` // Pubkey used for Signature
+	Signature string       `json:"signature"` // Client signature
+
+	// Metadata generated by server
+	ParentID  uint32 `json:"parentid"`  // Parent comment ID
+	UserID    string `json:"userid"`    // Author user ID
+	Timestamp int64  `json:"timestamp"` // Received UNIX timestamp
+	Receipt   string `json:"receipt"`   // Server sig of client sig
+}
+
+// VoteT represents a comment upvote/downvote.
+type VoteT int32
+
+const (
+	// VoteInvalid is an invalid comment vote.
+	VoteInvalid VoteT = 0
+
+	// VoteDownvote represents a comment downvote.
+	VoteDownvote VoteT = -1
+
+	// VoteUpvote represents a comment upvote.
+	VoteUpvote VoteT = 1
+)
+
+// CommentVote is the structure that is saved to disk when a comment is voted
+// on.
+//
+// Signature is the client signature of the State+Token+CommentID+Vote.
+type CommentVote struct {
+	// Data generated by client
+	UserID    string       `json:"userid"`    // Unique user ID
+	State     RecordStateT `json:"state"`     // Record state
+	Token     string       `json:"token"`     // Record token
+	CommentID uint32       `json:"commentid"` // Comment ID
+	Vote      VoteT        `json:"vote"`      // Upvote or downvote
+	PublicKey string       `json:"publickey"` // Public key used for signature
+	Signature string       `json:"signature"` // Client signature
+
+	// Metadata generated by server
+	Timestamp int64  `json:"timestamp"` // Received UNIX timestamp
+	Receipt   string `json:"receipt"`   // Server signature of client signature
+}
+
+// New creates a new comment.
+//
+// The parent ID is used to reply to an existing comment. A parent ID of 0
+// indicates that the comment is a base level comment and not a reply commment.
+//
+// Signature is the client signature of State+Token+ParentID+Comment.
+type New struct {
+	UserID    string       `json:"userid"`    // Unique user ID
+	State     RecordStateT `json:"state"`     // Record state
+	Token     string       `json:"token"`     // Record token
+	ParentID  uint32       `json:"parentid"`  // Parent comment ID
+	Comment   string       `json:"comment"`   // Comment text
+	PublicKey string       `json:"publickey"` // Pubkey used for Signature
+	Signature string       `json:"signature"` // Client signature
+
+	// Optional fields to be used freely
+	ExtraData     string `json:"extradata,omitempty"`
+	ExtraDataHint string `json:"extradatahint,omitempty"`
+}
+
+// NewReply is the reply to the New command.
+type NewReply struct {
+	Comment Comment `json:"comment"`
+}
+
+// Edit edits an existing comment.
+//
+// Signature is the client signature of State+Token+ParentID+Comment.
+type Edit struct {
+	UserID    string       `json:"userid"`    // Unique user ID
+	State     RecordStateT `json:"state"`     // Record state
+	Token     string       `json:"token"`     // Record token
+	ParentID  uint32       `json:"parentid"`  // Parent comment ID
+	CommentID uint32       `json:"commentid"` // Comment ID
+	Comment   string       `json:"comment"`   // Comment text
+	PublicKey string       `json:"publickey"` // Pubkey used for Signature
+	Signature string       `json:"signature"` // Client signature
+
+	// Optional fields to be used freely
+	ExtraData     string `json:"extradata,omitempty"`
+	ExtraDataHint string `json:"extradatahint,omitempty"`
+}
+
+// EditReply is the reply to the Edit command.
+type EditReply struct {
+	Comment Comment `json:"comment"`
+}
+
+// Del permanently deletes all versions of the provided comment.
+//
+// Signature is the client signature of the State+Token+CommentID+Reason
+type Del struct {
+	State     RecordStateT `json:"state"`     // Record state
+	Token     string       `json:"token"`     // Record token
+	CommentID uint32       `json:"commentid"` // Comment ID
+	Reason    string       `json:"reason"`    // Reason for deletion
+	PublicKey string       `json:"publickey"` // Public key used for signature
+	Signature string       `json:"signature"` // Client signature
+}
+
+// DelReply is the reply to the Del command.
+type DelReply struct {
+	Comment Comment `json:"comment"`
+}
+
+// Vote casts a comment vote (upvote or downvote).
+//
+// The effect of a new vote on a comment score depends on the previous vote
+// from that user ID. Example, a user upvotes a comment that they have already
+// upvoted, the resulting vote score is 0 due to the second upvote removing the
+// original upvote. The public key cannot be relied on to remain the same for
+// each user so a user ID must be included.
+//
+// Signature is the client signature of the State+Token+CommentID+Vote.
+type Vote struct {
+	UserID    string       `json:"userid"`    // Unique user ID
+	State     RecordStateT `json:"state"`     // Record state
+	Token     string       `json:"token"`     // Record token
+	CommentID uint32       `json:"commentid"` // Comment ID
+	Vote      VoteT        `json:"vote"`      // Upvote or downvote
+	PublicKey string       `json:"publickey"` // Public key used for signature
+	Signature string       `json:"signature"` // Client signature
+}
+
+// VoteReply is the reply to the Vote command.
+type VoteReply struct {
+	Downvotes uint64 `json:"downvotes"` // Tolal downvotes on comment
+	Upvotes   uint64 `json:"upvotes"`   // Total upvotes on comment
+	Timestamp int64  `json:"timestamp"` // Received UNIX timestamp
+	Receipt   string `json:"receipt"`   // Server signature of client signature
+}
+
+// Get retrieves a batch of specified comments. The most recent version of each
+// comment is returned. An error is not returned if a comment is not found for
+// one or more of the comment IDs. Those entries will simply not be included in
+// the reply.
+type Get struct {
+	CommentIDs []uint32 `json:"commentids"`
+}
+
+// GetReply is the reply to the Get command. The returned map will not include
+// an entry for any comment IDs that did not correspond to an actual comment.
+// It is the responsibility of the caller to ensure that a comment was returned
+// for all of the provided comment IDs.
+type GetReply struct {
+	Comments map[uint32]Comment `json:"comments"` // [commentID]Comment
+}
+
+// GetAll retrieves all comments for a record. The latest version of each
+// comment is returned.
+type GetAll struct{}
+
+// GetAllReply is the reply to the GetAll command. The returned comments array
+// is ordered by comment ID from smallest to largest.
+type GetAllReply struct {
+	Comments []Comment `json:"comments"`
+}
+
+// GetVersion retrieves the specified version of a comment.
+type GetVersion struct {
+	CommentID uint32 `json:"commentid"`
+	Version   uint32 `json:"version"`
+}
+
+// GetVersionReply is the reply to the GetVersion command.
+type GetVersionReply struct {
+	Comment Comment `json:"comment"`
+}
+
+// Count retrieves the comments count for a record. The comments count is the
+// number of comments that have been made on a record.
+type Count struct{}
+
+// CountReply is the reply to the Count command.
+type CountReply struct {
+	Count uint32 `json:"count"`
+}
+
+// Votes retrieves the comment votes that meet the provided filtering criteria.
+type Votes struct {
+	UserID string `json:"userid"`
+}
+
+// VotesReply is the reply to the Votes command.
+type VotesReply struct {
+	Votes []CommentVote `json:"votes"`
+}
+
+// Proof contains an inclusion proof for the digest in the merkle root. The
+// ExtraData field is used by certain types of proofs to include additional
+// data that is required to validate the proof.
+type Proof struct {
+	Type       string   `json:"type"`
+	Digest     string   `json:"digest"`
+	MerkleRoot string   `json:"merkleroot"`
+	MerklePath []string `json:"merklepath"`
+	ExtraData  string   `json:"extradata"` // JSON encoded
+}
+
+// Timestamp contains all of the data required to verify that a piece of data
+// was timestamped onto the decred blockchain.
+//
+// All digests are hex encoded SHA256 digests. The merkle root can be found in
+// the OP_RETURN of the specified DCR transaction.
+//
+// TxID, MerkleRoot, and Proofs will only be populated once the merkle root has
+// been included in a DCR tx and the tx has 6 confirmations. The Data field
+// will not be populated if the data has been censored.
+type Timestamp struct {
+	Data       string  `json:"data"` // JSON encoded
+	Digest     string  `json:"digest"`
+	TxID       string  `json:"txid"`
+	MerkleRoot string  `json:"merkleroot"`
+	Proofs     []Proof `json:"proofs"`
+}
+
+// Timestamps retrieves the timestamps for a record's comments. If no comment
+// IDs are provided then timestamps for all comments made on the record will
+// be returned. If IncludeVotes is set to true then the timestamps for the
+// comment votes will also be returned. If a provided comment ID does not
+// exist then it will not be included in the reply.
+type Timestamps struct {
+	CommentIDs   []uint32 `json:"commentids,omitempty"`
+	IncludeVotes bool     `json:"includevotes,omitempty"`
+}
+
+// TimestampsReply is the reply to the timestamps command.
+type TimestampsReply struct {
+	Comments map[uint32][]Timestamp `json:"comments"`
+	Votes    map[uint32][]Timestamp `json:"votes"`
+}

politeiad/backendv2/tstorebe/plugins/comments/comments.go
@@ -1,0 +1,171 @@
+// Copyright (c) 2020-2021 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package comments
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+
+	"github.com/decred/politeia/politeiad/api/v1/identity"
+	backend "github.com/decred/politeia/politeiad/backendv2"
+	"github.com/decred/politeia/politeiad/backendv2/tstorebe/plugins"
+	"github.com/decred/politeia/politeiad/plugins/comments"
+)
+
+var (
+	_ plugins.PluginClient = (*commentsPlugin)(nil)
+)
+
+// commentsPlugin is the tstore backend implementation of the comments plugin.
+// The comments plugin extends a record with comment functionality.
+//
+// commentsPlugin satisfies the plugins PluginClient interface.
+type commentsPlugin struct {
+	sync.RWMutex
+	tstore plugins.TstoreClient
+
+	// dataDir is the comments plugin data directory. The only data
+	// that is stored here is cached data that can be re-created at any
+	// time by walking the trillian trees.
+	dataDir string
+
+	// identity contains the full identity that the plugin uses to
+	// create receipts, i.e. signatures of user provided data that
+	// prove the backend received and processed a plugin command.
+	identity *identity.FullIdentity
+
+	// Plugin settings
+	commentLengthMax uint32
+	voteChangesMax   uint32
+}
+
+// Setup performs any plugin setup that is required.
+//
+// This function satisfies the plugins PluginClient interface.
+func (p *commentsPlugin) Setup() error {
+	log.Tracef("comments Setup")
+
+	return nil
+}
+
+// Cmd executes a plugin command.
+//
+// This function satisfies the plugins PluginClient interface.
+func (p *commentsPlugin) Cmd(token []byte, cmd, payload string) (string, error) {
+	log.Tracef("comments Cmd: %x %v %v", token, cmd, payload)
+
+	switch cmd {
+	case comments.CmdNew:
+		return p.cmdNew(token, payload)
+	case comments.CmdEdit:
+		return p.cmdEdit(token, payload)
+	case comments.CmdDel:
+		return p.cmdDel(token, payload)
+	case comments.CmdVote:
+		return p.cmdVote(token, payload)
+	case comments.CmdGet:
+		return p.cmdGet(token, payload)
+	case comments.CmdGetAll:
+		return p.cmdGetAll(token)
+	case comments.CmdGetVersion:
+		return p.cmdGetVersion(token, payload)
+	case comments.CmdCount:
+		return p.cmdCount(token)
+	case comments.CmdVotes:
+		return p.cmdVotes(token, payload)
+	case comments.CmdTimestamps:
+		return p.cmdTimestamps(token, payload)
+	}
+
+	return "", backend.ErrPluginCmdInvalid
+}
+
+// Hook executes a plugin hook.
+//
+// This function satisfies the plugins PluginClient interface.
+func (p *commentsPlugin) Hook(h plugins.HookT, payload string) error {
+	log.Tracef("comments Hook: %x %v", plugins.Hooks[h])
+
+	return nil
+}
+
+// Fsck performs a plugin filesystem check.
+//
+// This function satisfies the plugins PluginClient interface.
+func (p *commentsPlugin) Fsck() error {
+	log.Tracef("comments Fsck")
+
+	// Verify record index coherency
+	// Verify CommentDel blobs were actually deleted
+
+	return nil
+}
+
+// Settings returns the plugin settings.
+//
+// This function satisfies the plugins PluginClient interface.
+func (p *commentsPlugin) Settings() []backend.PluginSetting {
+	log.Tracef("comments Settings")
+
+	return []backend.PluginSetting{
+		{
+			Key:   comments.SettingKeyCommentLengthMax,
+			Value: strconv.FormatUint(uint64(p.commentLengthMax), 10),
+		},
+		{
+			Key:   comments.SettingKeyVoteChangesMax,
+			Value: strconv.FormatUint(uint64(p.voteChangesMax), 10),
+		},
+	}
+}
+
+// New returns a new comments plugin.
+func New(tstore plugins.TstoreClient, settings []backend.PluginSetting, dataDir string, id *identity.FullIdentity) (*commentsPlugin, error) {
+	// Setup comments plugin data dir
+	dataDir = filepath.Join(dataDir, comments.PluginID)
+	err := os.MkdirAll(dataDir, 0700)
+	if err != nil {
+		return nil, err
+	}
+
+	// Default plugin settings
+	var (
+		commentLengthMax = comments.SettingCommentLengthMax
+		voteChangesMax   = comments.SettingVoteChangesMax
+	)
+
+	// Override defaults with any passed in settings
+	for _, v := range settings {
+		switch v.Key {
+		case comments.SettingKeyCommentLengthMax:
+			u, err := strconv.ParseUint(v.Value, 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid plugin setting %v '%v': %v",
+					v.Key, v.Value, err)
+			}
+			commentLengthMax = uint32(u)
+		case comments.SettingKeyVoteChangesMax:
+			u, err := strconv.ParseUint(v.Value, 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid plugin setting %v '%v': %v",
+					v.Key, v.Value, err)
+			}
+			voteChangesMax = uint32(u)
+		default:
+			return nil, fmt.Errorf("invalid comments plugin setting '%v'", v.Key)
+		}
+	}
+
+	return &commentsPlugin{
+		tstore:           tstore,
+		identity:         id,
+		dataDir:          dataDir,
+		commentLengthMax: commentLengthMax,
+		voteChangesMax:   voteChangesMax,
+	}, nil
+}

commit 422644c3688c65716bee862ae36c7d0a4088e6ad

comments: Fix empty comments bug.
This commit adds validation to the comments plugin that prevents empty
comments from being accepted.

This bug was introduced by 2e41e04.

politeiad/backendv2/tstorebe/plugins/comments/cmds.go
@@ -572,6 +572,12 @@ func (p *commentsPlugin) cmdNew(token []byte, payload string) (string, error) {
 	}
 
 	// Verify comment
+	if len(n.Comment) == 0 {
+		return "", backend.PluginError{
+			PluginID:  comments.PluginID,
+			ErrorCode: uint32(comments.ErrorCodeEmptyComment),
+		}
+	}
 	if len(n.Comment) > int(p.commentLengthMax) {
 		return "", backend.PluginError{
 			PluginID:  comments.PluginID,
@@ -708,6 +714,12 @@ func (p *commentsPlugin) cmdEdit(token []byte, payload string) (string, error) {
 	}
 
 	// Verify comment
+	if len(e.Comment) == 0 {
+		return "", backend.PluginError{
+			PluginID:  comments.PluginID,
+			ErrorCode: uint32(comments.ErrorCodeEmptyComment),
+		}
+	}
 	if len(e.Comment) > int(p.commentLengthMax) {
 		return "", backend.PluginError{
 			PluginID:  comments.PluginID,

politeiad/plugins/comments/comments.go
@@ -161,8 +161,14 @@ const (
 	// allowed.
 	ErrorCodeEditNotAllowed = 13
 
-	// ErrorCodeLast unit test only.
-	ErrorCodeLast ErrorCodeT = 14
+	// ErrorCodeEmptyComment is returned when a comment with no comment text
+	// is submitted.
+	ErrorCodeEmptyComment = 14
+
+	// ErrorCodeLast is used by unit tests to verify that all error codes have
+	// a human readable entry in the ErrorCodes map. This error code will never
+	// be returned.
+	ErrorCodeLast ErrorCodeT = 15
 )
 
 var (
@@ -182,6 +188,7 @@ var (
 		ErrorCodeRecordStateInvalid:     "record state invalid",
 		ErrorCodeExtraDataNotAllowed:    "comment extra data not allowed",
 		ErrorCodeEditNotAllowed:         "comment edit is not allowed",
+		ErrorCodeEmptyComment:           "comment is empty",
 	}
 )
 