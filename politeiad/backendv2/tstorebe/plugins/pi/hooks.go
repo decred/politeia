decred/politeia

commit b43eebdeb7d444632781683f9ed78b98cebfd3b7

multi: Add proposal author updates.
Proposal authors need to be able to give periodic updates on the status
of their proposal. These updates will be given as comments. The Politeia
users will have the ability to interact with the author and ask further
questions.

Before this commit, once a proposal vote has finished the proposal is
locked and no additional comments can be made. This commit extends this
behavior to the following:

 - Once a proposal vote has finished, all existing comment threads are
   locked.  When a proposal author wants to give an update on their
   approved proposal they could start a new comment thread.
   
 - The author is the only user that will have the ability to start a new
   comment thread once the voting period has finished.
   
 - Each update will have an author provided title.
 - Anyone can reply to any comments in the thread and can cast
   upvotes/downvotes for any comments in the thread.
   
 - The comment thread will remain open until either the author starts a
   new update thread or an admin marks the proposal as closed/completed.
   

**Implementation:**


  - Add the ProposalUpdateMetadata and ProposalUpdateHint to the pi
    plugin API and politeiawww API.
      
  - Use titleLengthMin, titleLengthMax & titleSupportedChars plugin
    settings to verify both the proposal name & the proposal update
    title.
      
  - Add validation to the pi plugin comment hooks that implements the
     behavior described above.
      
  - Update the pictl commentnew command to be able to handle proposal
    updates.

politeiad/backendv2/tstorebe/plugins/pi/hooks.go
@@ -16,6 +16,7 @@ import (
 	"github.com/decred/politeia/politeiad/plugins/comments"
 	"github.com/decred/politeia/politeiad/plugins/pi"
 	"github.com/decred/politeia/politeiad/plugins/ticketvote"
+	"github.com/decred/politeia/politeiad/plugins/usermd"
 	"github.com/decred/politeia/util"
 	"github.com/pkg/errors"
 )
@@ -93,20 +94,20 @@ func (p *piPlugin) hookEditRecordPre(payload string) error {
 
 // hookCommentNew adds pi specific validation onto the comments plugin New
 // command.
-func (p *piPlugin) hookCommentNew(token []byte) error {
-	return p.commentWritesAllowed(token)
+func (p *piPlugin) hookCommentNew(token []byte, cmd, payload string) error {
+	return p.commentWritesAllowed(token, cmd, payload)
 }
 
 // hookCommentDel adds pi specific validation onto the comments plugin Del
 // command.
-func (p *piPlugin) hookCommentDel(token []byte) error {
-	return p.commentWritesAllowed(token)
+func (p *piPlugin) hookCommentDel(token []byte, cmd, payload string) error {
+	return p.commentWritesAllowed(token, cmd, payload)
 }
 
 // hookCommentVote adds pi specific validation onto the comments plugin Vote
 // command.
-func (p *piPlugin) hookCommentVote(token []byte) error {
-	return p.commentWritesAllowed(token)
+func (p *piPlugin) hookCommentVote(token []byte, cmd, payload string) error {
+	return p.commentWritesAllowed(token, cmd, payload)
 }
 
 // hookPluginPre extends plugin write commands from other plugins with pi
@@ -124,21 +125,21 @@ func (p *piPlugin) hookPluginPre(payload string) error {
 	case comments.PluginID:
 		switch hpp.Cmd {
 		case comments.CmdNew:
-			return p.hookCommentNew(hpp.Token)
+			return p.hookCommentNew(hpp.Token, hpp.Cmd, hpp.Payload)
 		case comments.CmdDel:
-			return p.hookCommentDel(hpp.Token)
+			return p.hookCommentDel(hpp.Token, hpp.Cmd, hpp.Payload)
 		case comments.CmdVote:
-			return p.hookCommentVote(hpp.Token)
+			return p.hookCommentVote(hpp.Token, hpp.Cmd, hpp.Payload)
 		}
 	}
 
 	return nil
 }
 
-// proposalNameIsValid returns whether the provided name is a valid proposal
-// name.
-func (p *piPlugin) proposalNameIsValid(name string) bool {
-	return p.proposalNameRegexp.MatchString(name)
+// titleIsValid returns whether the provided title, which can be either a
+// proposal name or an author update title, matches the pi plugin title regex.
+func (p *piPlugin) titleIsValid(title string) bool {
+	return p.titleRegexp.MatchString(title)
 }
 
 // proposalStartDateIsValid returns whether the provided start date is valid.
@@ -317,11 +318,11 @@ func (p *piPlugin) proposalFilesVerify(files []backend.File) error {
 	}
 
 	// Verify proposal name
-	if !p.proposalNameIsValid(pm.Name) {
+	if !p.titleIsValid(pm.Name) {
 		return backend.PluginError{
 			PluginID:     pi.PluginID,
-			ErrorCode:    uint32(pi.ErrorCodeProposalNameInvalid),
-			ErrorContext: p.proposalNameRegexp.String(),
+			ErrorCode:    uint32(pi.ErrorCodeTitleInvalid),
+			ErrorContext: p.titleRegexp.String(),
 		}
 	}
 
@@ -390,11 +391,267 @@ func (p *piPlugin) voteSummary(token []byte) (*ticketvote.SummaryReply, error) {
 	return &sr, nil
 }
 
+// comments requests all comments on a record from the comments plugin.
+func (p *piPlugin) comments(token []byte) (*comments.GetAllReply, error) {
+	reply, err := p.backend.PluginRead(token, comments.PluginID,
+		comments.CmdGetAll, "")
+	if err != nil {
+		return nil, err
+	}
+	var gar comments.GetAllReply
+	err = json.Unmarshal([]byte(reply), &gar)
+	if err != nil {
+		return nil, err
+	}
+	return &gar, nil
+}
+
+// isInCommentTree returns whether the leafID is part of the provided comment
+// tree. A leaf is considered to be part of the tree if the leaf is a child of
+// the root or the leaf references the root itself.
+func isInCommentTree(rootID, leafID uint32, cs []comments.Comment) bool {
+	if leafID == rootID {
+		return true
+	}
+	// Convert comments slice to a map
+	commentsMap := make(map[uint32]comments.Comment, len(cs))
+	for _, c := range cs {
+		commentsMap[c.CommentID] = c
+	}
+
+	// Start with the provided comment leaf and traverse the comment tree up
+	// until either the provided root ID is found or we reach the tree head. The
+	// tree head will have a comment ID of 0.
+	current := commentsMap[leafID]
+	for current.ParentID != 0 {
+		// Check if next parent in the tree is the rootID.
+		if current.ParentID == rootID {
+			return true
+		}
+		leafID = current.ParentID
+		current = commentsMap[leafID]
+	}
+	return false
+}
+
+// latestAuthorUpdate gets the latest author update on a record, if
+// the record has no author update it returns nil.
+func latestAuthorUpdate(token []byte, cs []comments.Comment) *comments.Comment {
+	var latestAuthorUpdate comments.Comment
+	for _, c := range cs {
+		if c.ExtraDataHint != pi.ProposalUpdateHint {
+			continue
+		}
+		if c.Timestamp > latestAuthorUpdate.Timestamp {
+			latestAuthorUpdate = c
+		}
+	}
+	return &latestAuthorUpdate
+}
+
+// recordAuthor returns the author's userID of the record associated with
+// the provided token.
+func (p *piPlugin) recordAuthor(token []byte) (string, error) {
+	reply, err := p.backend.PluginRead(token, usermd.PluginID,
+		usermd.CmdAuthor, "")
+	if err != nil {
+		return "", err
+	}
+	var ar usermd.AuthorReply
+	err = json.Unmarshal([]byte(reply), &ar)
+	if err != nil {
+		return "", err
+	}
+	return ar.UserID, nil
+}
+
+// commentVoteAllowedOnApprovedProposal verifies that the given comment
+// vote is allowed on a proposal which finished voting and it's vote was
+// approved.
+func (p *piPlugin) commentVoteAllowedOnApprovedProposal(token []byte, payload string, latestAuthorUpdate comments.Comment, cs []comments.Comment) error {
+	// Decode payload
+	var v comments.Vote
+	err := json.Unmarshal([]byte(payload), &v)
+	if err != nil {
+		return err
+	}
+
+	if !isInCommentTree(latestAuthorUpdate.CommentID, v.CommentID, cs) {
+		return backend.PluginError{
+			PluginID:  pi.PluginID,
+			ErrorCode: uint32(pi.ErrorCodeCommentWriteNotAllowed),
+			ErrorContext: "votes are only allowed on the author's " +
+				"most recent update thread",
+		}
+	}
+
+	return nil
+}
+
+// isValidAuthorUpdate returns whether the given new comment is a valid author
+// update.
+//
+// The comment must include proper proposal update metadata and the comment
+// must be submitted by the proposal author for it to be considered a valid
+// author update.
+func (p *piPlugin) isValidAuthorUpdate(token []byte, n comments.New) error {
+	// Get the proposal author. The proposal author
+	// and the comment author must be the same user.
+	recordAuthorID, err := p.recordAuthor(token)
+	if err != nil {
+		return err
+	}
+	if recordAuthorID != n.UserID {
+		return backend.PluginError{
+			PluginID:     pi.PluginID,
+			ErrorCode:    uint32(pi.ErrorCodeCommentWriteNotAllowed),
+			ErrorContext: "user is not the proposal author",
+		}
+	}
+
+	// Verify extra data fields
+	if n.ExtraDataHint != pi.ProposalUpdateHint {
+		return backend.PluginError{
+			PluginID:  pi.PluginID,
+			ErrorCode: uint32(pi.ErrorCodeExtraDataHintInvalid),
+			ErrorContext: fmt.Sprintf("got %v, want %v",
+				n.ExtraDataHint, pi.ProposalUpdateHint),
+		}
+	}
+	var pum pi.ProposalUpdateMetadata
+	err = json.Unmarshal([]byte(n.ExtraData), &pum)
+	if err != nil {
+		return backend.PluginError{
+			PluginID:  pi.PluginID,
+			ErrorCode: uint32(pi.ErrorCodeExtraDataInvalid),
+		}
+	}
+
+	// Verify update title
+	if !p.titleIsValid(pum.Title) {
+		return backend.PluginError{
+			PluginID:     pi.PluginID,
+			ErrorCode:    uint32(pi.ErrorCodeTitleInvalid),
+			ErrorContext: p.titleRegexp.String(),
+		}
+	}
+
+	// The comment is a valid author update.
+	return nil
+}
+
+// commentNewAllowedOnApprovedProposal verifies that the given new comment
+// is allowed on a proposal which finished voting and it's vote was approved.
+func (p *piPlugin) commentNewAllowedOnApprovedProposal(token []byte, payload string, latestAuthorUpdate comments.Comment, cs []comments.Comment) error {
+	// Decode payload
+	var n comments.New
+	err := json.Unmarshal([]byte(payload), &n)
+	if err != nil {
+		return err
+	}
+
+	// A new comment on an approved proposal must either be an update
+	// from the author (parent ID will be 0) or a reply to the latest
+	// author update.
+	isUpdateReply := isInCommentTree(latestAuthorUpdate.CommentID,
+		n.ParentID, cs)
+	switch {
+	case n.ParentID == 0:
+		// This might be an update from the author.
+		return p.isValidAuthorUpdate(token, n)
+
+	case isUpdateReply:
+		// This is a reply to the latest update. This is allowed.
+		return nil
+
+	case !isUpdateReply:
+		// New comment is a reply, but is not a reply to the latest update. This
+		// is not allowed.
+		return backend.PluginError{
+			PluginID:  pi.PluginID,
+			ErrorCode: uint32(pi.ErrorCodeCommentWriteNotAllowed),
+			ErrorContext: "comment replies are only allowed on " +
+				"the author's most recent update thread",
+		}
+
+	default:
+		// This should not happen
+		return errors.Errorf("unknown comment write state")
+	}
+}
+
+// writesAllowedOnApprovedProposal verifies that the given comment write is
+// allowed on a proposal which finished voting and it's vote was approved. This
+// includes both comments and comment votes.
+func (p *piPlugin) writesAllowedOnApprovedProposal(token []byte, cmd, payload string) error {
+	// Get billing status to determine whether to allow author updates
+	// or not.
+	var bsc pi.BillingStatusChange
+	billingStatuses, err := p.billingStatuses(token)
+	if err != nil {
+		return err
+	}
+	// We assume here that admins can set a billing status only once
+	if len(billingStatuses) > 0 {
+		bsc = billingStatuses[0]
+		if bsc.Status == pi.BillingStatusClosed ||
+			bsc.Status == pi.BillingStatusCompleted {
+			// If billing status is set to closed or completed, comment writes
+			// are not allowed.
+			return backend.PluginError{
+				PluginID:  pi.PluginID,
+				ErrorCode: uint32(pi.ErrorCodeBillingStatusInvalid),
+				ErrorContext: "billing status is set to closed/completed;" +
+					" proposal is locked",
+			}
+		}
+	}
+
+	// Get latest proposal author update
+	gar, err := p.comments(token)
+	if err != nil {
+		return err
+	}
+	latestAuthorUpdate := latestAuthorUpdate(token, gar.Comments)
+
+	switch cmd {
+	// If the user is submitting a new comment then it must be either a new
+	// author update or a comment on the latest author update thread.
+	case comments.CmdNew:
+		return p.commentNewAllowedOnApprovedProposal(token, payload,
+			*latestAuthorUpdate, gar.Comments)
+
+	// If the user is voting on a comment then it must be on one of the latest
+	// author update thread comments.
+	case comments.CmdVote:
+		return p.commentVoteAllowedOnApprovedProposal(token, payload,
+			*latestAuthorUpdate, gar.Comments)
+
+	}
+
+	return nil
+}
+
 // commentWritesAllowed verifies that a proposal has a vote status that allows
 // comment writes to be made to the proposal. This includes both comments and
-// comment votes. Comment writes are allowed up until the proposal has finished
-// voting.
-func (p *piPlugin) commentWritesAllowed(token []byte) error {
+// comment votes.
+//
+// Once a proposal vote has finished, all existing comment threads are locked.
+//
+// When a proposal author wants to give an update on their **approved**
+// proposal they can start a new comment thread.
+//
+// The author is the only user that will have the ability to
+// start a new comment thread once the voting period has finished.
+//
+// Each update must have an author provided title.
+//
+// Anyone can reply to any comments in the thread and can cast
+// upvotes/downvotes for any comments in the thread.
+//
+// The comment thread will remain open until either the author starts a new
+// update thread or an admin marks the proposal as closed/completed.
+func (p *piPlugin) commentWritesAllowed(token []byte, cmd, payload string) error {
 	vs, err := p.voteSummary(token)
 	if err != nil {
 		return err
@@ -404,12 +661,16 @@ func (p *piPlugin) commentWritesAllowed(token []byte) error {
 		ticketvote.VoteStatusStarted:
 		// Comment writes are allowed on these vote statuses
 		return nil
+
+	case ticketvote.VoteStatusApproved:
+		return p.writesAllowedOnApprovedProposal(token, cmd, payload)
+
 	default:
 		// Vote status does not allow writes
 		return backend.PluginError{
 			PluginID:     pi.PluginID,
-			ErrorCode:    uint32(pi.ErrorCodeVoteStatusInvalid),
-			ErrorContext: "vote has ended; proposal is locked",
+			ErrorCode:    uint32(pi.ErrorCodeCommentWriteNotAllowed),
+			ErrorContext: "vote has ended; comments are locked",
 		}
 	}
 }

politeiad/backendv2/tstorebe/plugins/pi/hooks_test.go
@@ -20,9 +20,164 @@ import (
 
 	backend "github.com/decred/politeia/politeiad/backendv2"
 	"github.com/decred/politeia/politeiad/backendv2/tstorebe/plugins"
+	"github.com/decred/politeia/politeiad/plugins/comments"
 	"github.com/decred/politeia/politeiad/plugins/pi"
 	"github.com/decred/politeia/util"
 )
+
+func TestIsInCommentTree(t *testing.T) {
+	// Setup test data
+	oneNodeTree := []comments.Comment{
+		{
+			CommentID: 1,
+			ParentID:  0,
+		},
+	}
+
+	twoLeafsTree := []comments.Comment{
+		{
+			CommentID: 1,
+			ParentID:  0,
+		},
+		{
+			CommentID: 2,
+			ParentID:  0,
+		},
+	}
+
+	threeLevelsTree := []comments.Comment{
+		{
+			CommentID: 1,
+			ParentID:  0,
+		},
+		{
+			CommentID: 2,
+			ParentID:  1,
+		},
+		{
+			CommentID: 3,
+			ParentID:  2,
+		},
+		{
+			CommentID: 4,
+			ParentID:  0,
+		},
+	}
+
+	sixLevelsTree := []comments.Comment{
+		{
+			CommentID: 1,
+			ParentID:  0,
+		},
+		{
+			CommentID: 2,
+			ParentID:  1,
+		},
+		{
+			CommentID: 3,
+			ParentID:  1,
+		},
+		{
+			CommentID: 4,
+			ParentID:  2,
+		},
+		{
+			CommentID: 5,
+			ParentID:  2,
+		},
+		{
+			CommentID: 6,
+			ParentID:  3,
+		},
+		{
+			CommentID: 7,
+			ParentID:  5,
+		},
+		{
+			CommentID: 8,
+			ParentID:  5,
+		},
+		{
+			CommentID: 9,
+			ParentID:  8,
+		},
+		{
+			CommentID: 10,
+			ParentID:  9,
+		},
+	}
+
+	// Setup tests
+	var tests = []struct {
+		name string // Test name
+		rootID,
+		childID uint32
+		comments []comments.Comment
+		res      bool // Expected result
+	}{
+		{
+			name:     "one node tree true case",
+			rootID:   1,
+			childID:  1,
+			comments: oneNodeTree,
+			res:      true,
+		},
+		{
+			name:     "one node tree false case",
+			rootID:   0,
+			childID:  1,
+			comments: oneNodeTree,
+			res:      false,
+		},
+		{
+			name:     "two leafs tree false case",
+			rootID:   1,
+			childID:  2,
+			comments: twoLeafsTree,
+			res:      false,
+		},
+		{
+			name:     "three levels tree true case",
+			rootID:   1,
+			childID:  3,
+			comments: threeLevelsTree,
+			res:      true,
+		},
+		{
+			name:     "three levels tree false case",
+			rootID:   1,
+			childID:  4,
+			comments: threeLevelsTree,
+			res:      false,
+		},
+		{
+			name:     "six levels tree true case",
+			rootID:   1,
+			childID:  10,
+			comments: sixLevelsTree,
+			res:      true,
+		},
+		{
+			name:     "six levels tree false case",
+			rootID:   6,
+			childID:  10,
+			comments: sixLevelsTree,
+			res:      false,
+		},
+	}
+
+	// Run tests
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			res := isInCommentTree(tc.rootID, tc.childID, tc.comments)
+			if res != tc.res {
+				// Unexpected result
+				t.Errorf("unexpected result; wanted '%v', got '%v'", tc.res, res)
+				return
+			}
+		})
+	}
+}
 
 func TestHookNewRecordPre(t *testing.T) {
 	// Setup pi plugin
@@ -275,25 +430,25 @@ func proposalNameTests(t *testing.T) []proposalFormatTest {
 
 		b strings.Builder
 	)
-	for i := 0; i < int(pi.SettingProposalNameLengthMin)-1; i++ {
+	for i := 0; i < int(pi.SettingTitleLengthMin)-1; i++ {
 		b.WriteString("a")
 	}
 	nameTooShort = b.String()
 	b.Reset()
 
-	for i := 0; i < int(pi.SettingProposalNameLengthMax)+1; i++ {
+	for i := 0; i < int(pi.SettingTitleLengthMax)+1; i++ {
 		b.WriteString("a")
 	}
 	nameTooLong = b.String()
 	b.Reset()
 
-	for i := 0; i < int(pi.SettingProposalNameLengthMin); i++ {
+	for i := 0; i < int(pi.SettingTitleLengthMin); i++ {
 		b.WriteString("a")
 	}
 	nameMinLength = b.String()
 	b.Reset()
 
-	for i := 0; i < int(pi.SettingProposalNameLengthMax); i++ {
+	for i := 0; i < int(pi.SettingTitleLengthMax); i++ {
 		b.WriteString("a")
 	}
 	nameMaxLength = b.String()
@@ -329,7 +484,7 @@ func proposalNameTests(t *testing.T) []proposalFormatTest {
 	// fails.
 	errNameInvalid := backend.PluginError{
 		PluginID:  pi.PluginID,
-		ErrorCode: uint32(pi.ErrorCodeProposalNameInvalid),
+		ErrorCode: uint32(pi.ErrorCodeTitleInvalid),
 	}
 
 	return []proposalFormatTest{

politeiad/backendv2/tstorebe/plugins/pi/pi.go
@@ -43,20 +43,20 @@ type piPlugin struct {
 	identity *identity.FullIdentity
 
 	// Plugin settings
-	textFileCountMax           uint32
-	textFileSizeMax            uint32 // In bytes
-	imageFileCountMax          uint32
-	imageFileSizeMax           uint32 // In bytes
-	proposalNameSupportedChars string // JSON encoded []string
-	proposalNameLengthMin      uint32 // In characters
-	proposalNameLengthMax      uint32 // In characters
-	proposalNameRegexp         *regexp.Regexp
-	proposalAmountMin          uint64 // In cents
-	proposalAmountMax          uint64 // In cents
-	proposalStartDateMin       int64  // Seconds from current time
-	proposalEndDateMax         int64  // Seconds from current time
-	proposalDomainsEncoded     string // JSON encoded []string
-	proposalDomains            map[string]struct{}
+	textFileCountMax       uint32
+	textFileSizeMax        uint32 // In bytes
+	imageFileCountMax      uint32
+	imageFileSizeMax       uint32 // In bytes
+	titleSupportedChars    string // JSON encoded []string
+	titleLengthMin         uint32 // In characters
+	titleLengthMax         uint32 // In characters
+	titleRegexp            *regexp.Regexp
+	proposalAmountMin      uint64 // In cents
+	proposalAmountMax      uint64 // In cents
+	proposalStartDateMin   int64  // Seconds from current time
+	proposalEndDateMax     int64  // Seconds from current time
+	proposalDomainsEncoded string // JSON encoded []string
+	proposalDomains        map[string]struct{}
 }
 
 // Setup performs any plugin setup that is required.
@@ -133,16 +133,16 @@ func (p *piPlugin) Settings() []backend.PluginSetting {
 			Value: strconv.FormatUint(uint64(p.imageFileSizeMax), 10),
 		},
 		{
-			Key:   pi.SettingKeyProposalNameLengthMin,
-			Value: strconv.FormatUint(uint64(p.proposalNameLengthMin), 10),
+			Key:   pi.SettingKeyTitleLengthMin,
+			Value: strconv.FormatUint(uint64(p.titleLengthMin), 10),
 		},
 		{
-			Key:   pi.SettingKeyProposalNameLengthMax,
-			Value: strconv.FormatUint(uint64(p.proposalNameLengthMax), 10),
+			Key:   pi.SettingKeyTitleLengthMax,
+			Value: strconv.FormatUint(uint64(p.titleLengthMax), 10),
 		},
 		{
-			Key:   pi.SettingKeyProposalNameSupportedChars,
-			Value: p.proposalNameSupportedChars,
+			Key:   pi.SettingKeyTitleSupportedChars,
+			Value: p.titleSupportedChars,
 		},
 		{
 			Key:   pi.SettingKeyProposalAmountMin,
@@ -178,17 +178,17 @@ func New(backend backend.Backend, tstore plugins.TstoreClient, settings []backen
 
 	// Setup plugin setting default values
 	var (
-		textFileSizeMax    = pi.SettingTextFileSizeMax
-		imageFileCountMax  = pi.SettingImageFileCountMax
-		imageFileSizeMax   = pi.SettingImageFileSizeMax
-		nameLengthMin      = pi.SettingProposalNameLengthMin
-		nameLengthMax      = pi.SettingProposalNameLengthMax
-		nameSupportedChars = pi.SettingProposalNameSupportedChars
-		amountMin          = pi.SettingProposalAmountMin
-		amountMax          = pi.SettingProposalAmountMax
-		startDateMin       = pi.SettingProposalStartDateMin
-		endDateMax         = pi.SettingProposalEndDateMax
-		domains            = pi.SettingProposalDomains
+		textFileSizeMax     = pi.SettingTextFileSizeMax
+		imageFileCountMax   = pi.SettingImageFileCountMax
+		imageFileSizeMax    = pi.SettingImageFileSizeMax
+		titleLengthMin      = pi.SettingTitleLengthMin
+		titleLengthMax      = pi.SettingTitleLengthMax
+		titleSupportedChars = pi.SettingTitleSupportedChars
+		amountMin           = pi.SettingProposalAmountMin
+		amountMax           = pi.SettingProposalAmountMax
+		startDateMin        = pi.SettingProposalStartDateMin
+		endDateMax          = pi.SettingProposalEndDateMax
+		domains             = pi.SettingProposalDomains
 	)
 
 	// Override defaults with any passed in settings
@@ -215,22 +215,22 @@ func New(backend backend.Backend, tstore plugins.TstoreClient, settings []backen
 					v.Key, v.Value, err)
 			}
 			imageFileSizeMax = uint32(u)
-		case pi.SettingKeyProposalNameLengthMin:
+		case pi.SettingKeyTitleLengthMin:
 			u, err := strconv.ParseUint(v.Value, 10, 64)
 			if err != nil {
 				return nil, fmt.Errorf("invalid plugin setting %v '%v': %v",
 					v.Key, v.Value, err)
 			}
-			nameLengthMin = uint32(u)
-		case pi.SettingKeyProposalNameLengthMax:
+			titleLengthMin = uint32(u)
+		case pi.SettingKeyTitleLengthMax:
 			u, err := strconv.ParseUint(v.Value, 10, 64)
 			if err != nil {
 				return nil, fmt.Errorf("invalid plugin setting %v '%v': %v",
 					v.Key, v.Value, err)
 			}
-			nameLengthMax = uint32(u)
-		case pi.SettingKeyProposalNameSupportedChars:
-			err := json.Unmarshal([]byte(v.Value), &nameSupportedChars)
+			titleLengthMax = uint32(u)
+		case pi.SettingKeyTitleSupportedChars:
+			err := json.Unmarshal([]byte(v.Value), &titleSupportedChars)
 			if err != nil {
 				return nil, fmt.Errorf("invalid plugin setting %v '%v': %v",
 					v.Key, v.Value, err)
@@ -273,20 +273,20 @@ func New(backend backend.Backend, tstore plugins.TstoreClient, settings []backen
 		}
 	}
 
-	// Setup proposal name regex
-	rexp, err := util.Regexp(nameSupportedChars, uint64(nameLengthMin),
-		uint64(nameLengthMax))
+	// Setup title regex
+	rexp, err := util.Regexp(titleSupportedChars, uint64(titleLengthMin),
+		uint64(titleLengthMax))
 	if err != nil {
 		return nil, fmt.Errorf("proposal name regexp: %v", err)
 	}
 
-	// Encode the supported chars so that they can be returned as a
-	// plugin setting string.
-	b, err := json.Marshal(nameSupportedChars)
+	// Encode the title supported chars so that they
+	// can be returned as a plugin setting string.
+	b, err := json.Marshal(titleSupportedChars)
 	if err != nil {
 		return nil, err
 	}
-	nameSupportedCharsString := string(b)
+	titleSupportedCharsString := string(b)
 
 	// Encode the proposal domains so that they can be returned as a
 	// plugin setting string.
@@ -303,22 +303,22 @@ func New(backend backend.Backend, tstore plugins.TstoreClient, settings []backen
 	}
 
 	return &piPlugin{
-		dataDir:                    dataDir,
-		identity:                   id,
-		backend:                    backend,
-		textFileSizeMax:            textFileSizeMax,
-		tstore:                     tstore,
-		imageFileCountMax:          imageFileCountMax,
-		imageFileSizeMax:           imageFileSizeMax,
-		proposalNameLengthMin:      nameLengthMin,
-		proposalNameLengthMax:      nameLengthMax,
-		proposalNameSupportedChars: nameSupportedCharsString,
-		proposalNameRegexp:         rexp,
-		proposalAmountMin:          amountMin,
-		proposalAmountMax:          amountMax,
-		proposalStartDateMin:       startDateMin,
-		proposalEndDateMax:         endDateMax,
-		proposalDomainsEncoded:     domainsString,
-		proposalDomains:            domainsMap,
+		dataDir:                dataDir,
+		identity:               id,
+		backend:                backend,
+		textFileSizeMax:        textFileSizeMax,
+		tstore:                 tstore,
+		imageFileCountMax:      imageFileCountMax,
+		imageFileSizeMax:       imageFileSizeMax,
+		titleLengthMin:         titleLengthMin,
+		titleLengthMax:         titleLengthMax,
+		titleSupportedChars:    titleSupportedCharsString,
+		titleRegexp:            rexp,
+		proposalAmountMin:      amountMin,
+		proposalAmountMax:      amountMax,
+		proposalStartDateMin:   startDateMin,
+		proposalEndDateMax:     endDateMax,
+		proposalDomainsEncoded: domainsString,
+		proposalDomains:        domainsMap,
 	}, nil
 }

commit 867deae62022a88b40bc57333916a372b4c45680

plugins/pi: Fix author update ordering bug.
This commit fixes a bug in a function that was used to determine the
most recent author update. The function was using the wrong timestamp to
determine the order of the author updates. It was using the timestamp
from of the most recent comment edit when it should have been using the
timestamp of the comment creation.

This bug was introduced by b43eebdeb.

politeiad/backendv2/tstorebe/plugins/pi/hooks.go
@@ -439,21 +439,6 @@ func isInCommentTree(rootID, leafID uint32, cs []comments.Comment) bool {
 	return false
 }
 
-// latestAuthorUpdate gets the latest author update on a record, if
-// the record has no author update it returns nil.
-func latestAuthorUpdate(token []byte, cs []comments.Comment) *comments.Comment {
-	var latestAuthorUpdate comments.Comment
-	for _, c := range cs {
-		if c.ExtraDataHint != pi.ProposalUpdateHint {
-			continue
-		}
-		if c.Timestamp > latestAuthorUpdate.Timestamp {
-			latestAuthorUpdate = c
-		}
-	}
-	return &latestAuthorUpdate
-}
-
 // recordAuthor returns the author's userID of the record associated with
 // the provided token.
 func (p *piPlugin) recordAuthor(token []byte) (string, error) {
@@ -750,4 +735,27 @@ func voteMetadataDecode(files []backend.File) (*ticketvote.VoteMetadata, error) 
 		break
 	}
 	return voteMD, nil
+}
+
+// latestAuthorUpdate returns the most recent author update comment. If no
+// author updates are present, nil is returned.
+func latestAuthorUpdate(token []byte, cs []comments.Comment) *comments.Comment {
+	var update comments.Comment
+	for _, c := range cs {
+		switch {
+		case c.ExtraDataHint != pi.ProposalUpdateHint:
+			// This is not an author update comment
+			continue
+
+		case c.CreatedAt < update.CreatedAt:
+			// This is an author update comment, but is
+			// not the most recent one.
+			continue
+		}
+
+		// We've found a new author update comment
+		update = c
+	}
+
+	return &update
 }